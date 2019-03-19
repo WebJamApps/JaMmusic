0314e209a31410d02a6bfda3b49966452d702b26
dannymcwaves <dannymcwaves96@gmail.com>
2019-03-19 10:43:29 +0000

    import styles into the application

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ import { render } from 'react-dom';
 import App from './src';
 import store from './src/store';
 
+import './static/styles.css';
 
 render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
