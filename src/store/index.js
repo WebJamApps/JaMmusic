51b35ff48ab95a3e2e18bdc5e4a32ff8b62d9f88
dannymcwaves <dannymcwaves96@gmail.com>
2019-03-20 10:52:31 +0000

    add new keys to the store

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,13 @@ import logger from 'redux-logger';
 import appState from './reducers';
 
 
-const store = { name: 'Web JAM LLC' };
+const store = {
+  name: 'Web JAM LLC',
+  menu: '',
+  fullMenu: true,
+  role: '',
+  widescreen: true
+};
 
 
 export default createStore(appState, store, applyMiddleware(logger));
