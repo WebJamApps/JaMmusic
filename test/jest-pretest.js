ea1623ae8ca32debe229ad584f5b6b4202a795d3
dannymcwaves <dannymcwaves96@gmail.com>
2019-03-23 13:18:14 +0000

    some eslint updates

--- test/jest-pretest.js
+++ test/jest-pretest.js
@@ -1,6 +1,4 @@
-import 'aurelia-polyfills';
-import { Options } from 'aurelia-loader-nodejs';
-import { globalize } from 'aurelia-pal-nodejs';
+
 import * as path from 'path';
 
 Options.relativeToDir = path.join(__dirname, 'react-unit');
