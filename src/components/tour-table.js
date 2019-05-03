26a6783b040ac95c2785f531d3ef589956c68cde
Joshua Sherman <joshua.v.sherman@gmail.com>
2019-05-03 17:33:56 -0400

    need to import from json

--- src/components/tour-table.js
+++ src/components/tour-table.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react';
+import MUIDataTable from 'mui-datatables';
+import tour from '../tour';
 
 export class TourTable extends Component {// eslint-disable-line import/prefer-default-export
   constructor(props) {
     super(props);
+    this.tour = tour;
+    this.columns = [
+      {
+        name: 'Date',
+        label: 'Date',
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: 'Time',
+        label: 'Time',
+        options: {
+          filter: true,
+          sort: false
+        }
+      },
+      {
+        name: 'Location',
+        label: 'Location',
+        options: {
+          filter: true,
+          sort: false
+        }
+      },
+      {
+        name: 'Venue',
+        label: 'Venue',
+        options: {
+          filter: true,
+          sort: false
+        }
+      },
+      {
+        name: 'Tickets',
+        label: 'Tickets',
+        options: {
+          filter: true,
+          sort: false
+        }
+      }
+    ];
     this.data = {
       header: ['Date', 'Time', 'Location', 'Venue', 'Tickets'],
       body: [
@@ -25,122 +70,132 @@ export class TourTable extends Component {// eslint-disable-line import/prefer-d
   render() {
     return (
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th style={{ minWidth: '120px' }}>Date</th>
-              <th style={{ minWidth: '75px' }}>Time</th>
-              <th style={{ minWidth: '150px' }}>Location</th>
-              <th style={{ minWidth: '220px' }}>Venue</th>
-              <th style={{ minWidth: '120px' }}>Tickets</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Dec 24, 2018</td>
-              <td>4:30 pm</td>
-              <td>Salem, VA</td>
-              <td><a href="http://collegelutheran.org/" rel="noopener noreferrer" target="_blank">College Lutheran Church</a></td>
-              <td>Freewill</td>
-            </tr>
-            <tr>
-              <td>Dec 16, 2018</td>
-              <td>6:00 pm</td>
-              <td>Martinsville, VA</td>
-              <td>
-                <a href="https://stjoechurch.net/" rel="noopener noreferrer" target="_blank">St. Joseph Catholic Church</a>
+        <div style={{ maxWidth: '100%' }}>
+          <MUIDataTable
+            options={{ filterType: 'checkbox' }}
+            columns={this.columns}
+            data={this.tour}
+            title="Tour"
+          />
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th style={{ minWidth: '120px' }}>Date</th>
+                <th style={{ minWidth: '75px' }}>Time</th>
+                <th style={{ minWidth: '150px' }}>Location</th>
+                <th style={{ minWidth: '220px' }}>Venue</th>
+                <th style={{ minWidth: '120px' }}>Tickets</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Dec 24, 2018</td>
+                <td>4:30 pm</td>
+                <td>Salem, VA</td>
+                <td><a href="http://collegelutheran.org/" rel="noopener noreferrer" target="_blank">College Lutheran Church</a></td>
+                <td>Freewill</td>
+              </tr>
+              <tr>
+                <td>Dec 16, 2018</td>
+                <td>6:00 pm</td>
+                <td>Martinsville, VA</td>
+                <td>
+                  <a href="https://stjoechurch.net/" rel="noopener noreferrer" target="_blank">St. Joseph Catholic Church</a>
 , Christmas party
-              </td>
-              <td>Private</td>
-            </tr>
-            <tr>
-              <td>Dec 7, 2018</td>
-              <td>6:00 pm</td>
-              <td>Salem, VA</td>
-              <td>
-                <a href="http://collegelutheran.org/" rel="noopener noreferrer" target="_blank">College Lutheran Church</a>
-                {' '}
+                </td>
+                <td>Private</td>
+              </tr>
+              <tr>
+                <td>Dec 7, 2018</td>
+                <td>6:00 pm</td>
+                <td>Salem, VA</td>
+                <td>
+                  <a href="http://collegelutheran.org/" rel="noopener noreferrer" target="_blank">College Lutheran Church</a>
+                  {' '}
             front porch, prior to the Salem Christmas parade
-              </td>
-              <td>Free</td>
-            </tr>
-            <tr>
-              <td>Nov 29, 2018</td>
-              <td>5:45 pm</td>
-              <td>Salem, VA</td>
-              <td>Dinner to Honor Volunteers/Supporters of the Salem Clothes Closet</td>
-              <td>Private</td>
-            </tr>
-            <tr>
-              <td>Nov 24, 2018</td>
-              <td>4:45 pm</td>
-              <td>Moneta, VA</td>
-              <td><a href="http://www.resurrectioncatholic.org/" rel="noopener noreferrer" target="_blank">Resurrection Catholic Church</a></td>
-              <td>Freewill</td>
-            </tr>
-            <tr>
-              <td>Sept 29, 2018</td>
-              <td>5:30 pm</td>
-              <td>Marion, VA</td>
-              <td>
+                </td>
+                <td>Free</td>
+              </tr>
+              <tr>
+                <td>Nov 29, 2018</td>
+                <td>5:45 pm</td>
+                <td>Salem, VA</td>
+                <td>Dinner to Honor Volunteers/Supporters of the Salem Clothes Closet</td>
+                <td>Private</td>
+              </tr>
+              <tr>
+                <td>Nov 24, 2018</td>
+                <td>4:45 pm</td>
+                <td>Moneta, VA</td>
+                <td><a href="http://www.resurrectioncatholic.org/" rel="noopener noreferrer" target="_blank">Resurrection Catholic Church</a></td>
+                <td>Freewill</td>
+              </tr>
+              <tr>
+                <td>Sept 29, 2018</td>
+                <td>5:30 pm</td>
+                <td>Marion, VA</td>
+                <td>
 Hungry Mother Lutheran Retreat Center
-                <br />
-Beer & Brats Fundraiser -      
-                {' '}
-                {' '}
-                <a target="_blank" rel="noopener noreferrer" href="http://hungrymother.org/contact-us/">Contact</a>
-                {' '}
+                  <br />
+Beer & Brats Fundraiser -
+                  {' '}
+                  {' '}
+                  <a target="_blank" rel="noopener noreferrer" href="http://hungrymother.org/contact-us/">Contact</a>
+                  {' '}
 for more info.
-              </td>
-              <td><a target="_blank" rel="noopener noreferrer" href="http://hungrymother.org/give/">Donation</a></td>
-            </tr>
-            <tr>
-              <td>Sept 1, 2018</td>
-              <td>4:00 pm</td>
-              <td>Bent Mountain, VA</td>
-              <td>33rd Annual Pig Roast</td>
-              <td>Sold Out</td>
-            </tr>
-            <tr>
-              <td>Aug 4, 2018</td>
-              <td>9:00 am</td>
-              <td>Salem, VA</td>
-              <td><a href="https://www.facebook.com/SalemVaMarket/" rel="noopener noreferrer" target="_blank">Farmers Market</a></td>
-              <td>Free</td>
-            </tr>
-            <tr>
-              <td>July 30, 2018</td>
-              <td>6:00 pm</td>
-              <td>Salem, VA</td>
-              <td>
-                <a
-                  href="https://www.facebook.com/pages/biz/Majestic-Mic-at-Parkway-Brewery-1146829528673810/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
+                </td>
+                <td><a target="_blank" rel="noopener noreferrer" href="http://hungrymother.org/give/">Donation</a></td>
+              </tr>
+              <tr>
+                <td>Sept 1, 2018</td>
+                <td>4:00 pm</td>
+                <td>Bent Mountain, VA</td>
+                <td>33rd Annual Pig Roast</td>
+                <td>Sold Out</td>
+              </tr>
+              <tr>
+                <td>Aug 4, 2018</td>
+                <td>9:00 am</td>
+                <td>Salem, VA</td>
+                <td><a href="https://www.facebook.com/SalemVaMarket/" rel="noopener noreferrer" target="_blank">Farmers Market</a></td>
+                <td>Free</td>
+              </tr>
+              <tr>
+                <td>July 30, 2018</td>
+                <td>6:00 pm</td>
+                <td>Salem, VA</td>
+                <td>
+                  <a
+                    href="https://www.facebook.com/pages/biz/Majestic-Mic-at-Parkway-Brewery-1146829528673810/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
 Parkway Brewery
-                </a>
-              </td>
-              <td>Free</td>
-            </tr>
-            <tr>
-              <td>July 21, 2018</td>
-              <td>5:00 pm</td>
-              <td>Salem, VA</td>
-              <td>
-                <a href="https://www.facebook.com/events/199874127371694/" rel="noopener noreferrer" target="_blank">Web Jam LLC</a>
-              </td>
-              <td>Private</td>
-            </tr>
-            <tr>
-              <td>July 15, 2018</td>
-              <td>9:45 am</td>
-              <td>Salem, VA</td>
-              <td><a href="http://collegelutheran.org" rel="noopener noreferrer" target="_blank">College Lutheran Church</a></td>
-              <td>Freewill</td>
-            </tr>
-          </tbody>
-        </table>
+                  </a>
+                </td>
+                <td>Free</td>
+              </tr>
+              <tr>
+                <td>July 21, 2018</td>
+                <td>5:00 pm</td>
+                <td>Salem, VA</td>
+                <td>
+                  <a href="https://www.facebook.com/events/199874127371694/" rel="noopener noreferrer" target="_blank">Web Jam LLC</a>
+                </td>
+                <td>Private</td>
+              </tr>
+              <tr>
+                <td>July 15, 2018</td>
+                <td>9:45 am</td>
+                <td>Salem, VA</td>
+                <td><a href="http://collegelutheran.org" rel="noopener noreferrer" target="_blank">College Lutheran Church</a></td>
+                <td>Freewill</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
