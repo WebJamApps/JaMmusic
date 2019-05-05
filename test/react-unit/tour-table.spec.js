952b848e0bc9c68b71880bae67f71de015cedfcb
Joshua Sherman <joshua.v.sherman@gmail.com>
2019-05-04 20:41:14 -0400

    unit tests and cc

--- test/react-unit/tour-table.spec.js
+++ test/react-unit/tour-table.spec.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { TourTable } from '../../src/components/tour-table';
 
+const sinon = require('sinon');
+
 function setup() {
   const props = {};
   const wrapper = shallow(<TourTable />);
@@ -11,7 +13,37 @@ function setup() {
 describe('tour-table component test', () => {
   it('renders the component', () => {
     const { wrapper } = setup();
-    // console.log(wrapper.debug());
     expect(wrapper.find('.tourTable').exists()).toBe(true);
+  });
+  it('returns the json data', async () => {
+    const tt = new TourTable();
+    const jMock = sinon.mock(tt.jquery);
+    jMock.expects('getJSON').resolves([{
+      Date: 'Dec 7, 2018',
+      Time: '6:00 pm',
+      Location: 'Salem, VA',
+      Venue: 'College Lutheran Church front porch, prior to the Salem Christmas parade',
+      Tickets: 'Free'
+    }]);
+    const sMock = sinon.mock(tt);
+    sMock.expects('setState').returns(true);
+    let result;
+    try {
+      result = await tt.fetchJson();
+    } catch (e) { throw e; }
+    expect(result).toBe(true);
+    jMock.restore();
+    sMock.restore();
+  });
+  it('returns the json data error', async () => {
+    const tt = new TourTable();
+    const jMock = sinon.mock(tt.jquery);
+    jMock.expects('getJSON').rejects(new Error('bad'));
+    let result;
+    try {
+      result = await tt.fetchJson();
+    } catch (e) { throw e; }
+    expect(result).toBe(false);
+    jMock.restore();
   });
 });
