d587c5bdc07ec7c499bd92228cb0661a44d0cfda
dannymcwaves <dannymcwaves96@gmail.com>
2019-05-22 10:10:05 +0000

    update the music player tests to a 100% coverage

--- test/react-unit/music-player.spec.js
+++ test/react-unit/music-player.spec.js
@@ -31,15 +31,29 @@ describe('Music player component init', () => {
     expect(wrapper.instance().state.player.playing).toBe(false);
   });
 
-  it('should find and simulate shuffle', () => {
+  it('should find and simulate shuffle and confirm shuffling is on', () => {
     const { wrapper } = setup();
     wrapper.find('button#shuffle').simulate('click');
     expect(wrapper.instance().state.player.isShuffleOn).toBe(true);
   });
   
+  it('should find and simulate stop shuffle and confirm shuffling is off', () => {
+    const { wrapper } = setup();
+    wrapper.instance().setState({ index: 100 });
+    wrapper.instance().playEnd();
+    expect(wrapper.instance().state.index).toBe(0);
+  });
+  
+  it('should test the end of the player', () => {
+    const { wrapper } = setup();
+    wrapper.instance().setState({ player: { isShuffleOn: true } });
+    wrapper.find('button#shuffle').simulate('click');
+    expect(wrapper.instance().state.player.isShuffleOn).toBe(false);
+  });
+  
   it('should find and simulate a next song function', () => {
     const { wrapper } = setup();
-    wrapper.find('button#next').simulate('click');
+    wrapper.instance().next();
     expect(wrapper.instance().state.index).toBe(1);
   });
   
@@ -49,11 +63,38 @@ describe('Music player component init', () => {
     expect(wrapper.instance().state.index).not.toBe(0);
   });
   
+  it('should test previous song which has an index > 0', () => {
+    const { wrapper } = setup();
+    wrapper.instance().setState({ index: 1 });
+    wrapper.instance().prev();
+    expect(wrapper.instance().state.index).toBe(0);
+  });
+  
   it('should find and copy a playing song url', () => {
     const { wrapper } = setup();
     wrapper.instance().navigator = { clipboard: { async writeText(arg) { return arg; } } };
     wrapper.update();
     wrapper.find('#copyButton').simulate('click');
+  
+    expect(wrapper.instance().state.player.displayCopier).toBe('none');
+  });
+  
+  it('should hide copier message after showing for 1.5s', (done) => {
+    const { wrapper } = setup();
+    wrapper.instance().navigator = { clipboard: { async writeText(arg) { return arg; } } };
+    wrapper.update();
+    wrapper.instance().copyShare();
+
+    expect(wrapper.instance().state.player.displayCopier).toBe('none');
+    
+    setTimeout(() => done(), 1501);
+  });
+
+  it('should toggle off copying/share pane', () => {
+    const { wrapper } = setup();
+    wrapper.instance().setState({ player: { displayCopier: 'block' } });
+    wrapper.instance().share();
+    expect(wrapper.instance().state.player.displayCopier).toBe('none');
   });
 
   it('should test props passed to Music Player', () => {
