538b13e2601f87d02ce96a56389a6403e9d62308
dannymcwaves <dannymcwaves96@gmail.com>
2019-05-22 10:10:28 +0000

    test tour table custom rendering function

--- test/react-unit/tour-table.spec.js
+++ test/react-unit/tour-table.spec.js
@@ -15,16 +15,32 @@ describe('tour-table component test', () => {
     const { wrapper } = setup();
     expect(wrapper.find('.tourTable').exists()).toBe(true);
   });
+  it('tests the tour-table columns', () => {
+    const { wrapper } = setup();
+    expect(typeof wrapper.instance().setColumns).toBe('function');
+    wrapper.instance().setColumns();
+    wrapper.instance().columns[0].options.customBodyRender('<a href="http://collegelutheran.org/"'
+      + ' rel="noopener noreferrer" target="_blank">College Lutheran Church</a>');
+  });
   it('returns the json data', async () => {
     const tt = new TourTable();
     const jMock = sinon.mock(tt.jquery);
-    jMock.expects('getJSON').resolves([{
-      Date: 'Dec 7, 2018',
-      Time: '6:00 pm',
-      Location: 'Salem, VA',
-      Venue: 'College Lutheran Church front porch, prior to the Salem Christmas parade',
-      Tickets: 'Free',
-    }]);
+    jMock.expects('getJSON').resolves([
+      {
+        Date: 'Dec 7, 2018',
+        Time: '6:00 pm',
+        Location: 'Salem, VA',
+        Venue: 'College Lutheran Church front porch, prior to the Salem Christmas parade',
+        Tickets: 'Free',
+      },
+      {
+        Date: 'Dec 24, 2018',
+        Location: 'Salem, VA',
+        Tickets: 'Freewill',
+        Time: '4:30 pm',
+        Venue: '<a href="http://collegelutheran.org/" rel="noopener noreferrer" target="_blank">College Lutheran Church</a>',
+      },
+    ]);
     const sMock = sinon.mock(tt);
     sMock.expects('setState').returns(true);
     let result;
