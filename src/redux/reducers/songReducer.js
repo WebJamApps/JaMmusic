b91c5a12c4fa63da2a1cb2309ad87b9a2f6c7154
JoshuaVSherman <joshua.v.sherman@gmail.com>
2019-06-02 14:50:53 -0400

    unit test

--- src/redux/reducers/songReducer.js
+++ src/redux/reducers/songReducer.js
@@ -6,25 +6,10 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'GOT_SONGS':
-      // console.log(action.data);// eslint-disable-line no-console
       return Object.assign({}, state, {
         songs: action.data,
         error: '',
       });
-    // case 'LOGOUT':
-    //   return Object.assign({}, state, {
-    //     isAuthenticated: false,
-    //     email: '',
-    //     token: '',
-    //     error: '',
-    //   });
-    // case 'AUTH_ERROR':
-    //   return Object.assign({}, state, {
-    //     isAuthenticated: false,
-    //     email: '',
-    //     token: '',
-    //     error: action.error.message,
-    //   });
     default:
       return state;
   }
