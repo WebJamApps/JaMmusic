be11c3fdb57f2ac547db86a11991ca21bffd79f4
Rhayven <amayis@ymail.com>
2019-07-02 13:14:31 -0400

    WIP Updating Babel

--- test/puppeteer/google.spec.js
+++ test/puppeteer/google.spec.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+import 'core-js';
+import 'regenerator-runtime/runtime';
 
 describe('Google', () => {
   // beforeAll(async () => {
--- test/puppeteer/home.spec.js
+++ test/puppeteer/home.spec.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 describe('Homepage', () => {
   // beforeAll(async () => {
@@ -10,11 +11,15 @@ describe('Homepage', () => {
     await expect(page.title()).resolves.toMatch('Music | Web Jam LLC');
   });
   it('should goto subroute buymusic', async () => {
-    await page.goto('http://localhost:7878/music/buymusic', { waitUntil: 'load' });
+    await page.goto('http://localhost:7878/music/buymusic', {
+      waitUntil: 'load'
+    });
     await expect(page.title()).resolves.toMatch('Buy Music | Web Jam LLC');
   });
   it('should goto subroute originals', async () => {
-    await page.goto('http://localhost:7878/music/originals', { waitUntil: 'load' });
+    await page.goto('http://localhost:7878/music/originals', {
+      waitUntil: 'load'
+    });
     await expect(page.title()).resolves.toMatch('Original Songs | Web Jam LLC');
   });
 });
