eba788074b0e09edd9398e8cfe36f163a0d91a7d
Rhayven <amayis@ymail.com>
2019-07-19 03:42:37 -0400

    Updated puppeteer test

--- test/puppeteer/home.spec.js
+++ test/puppeteer/home.spec.js
@@ -3,7 +3,7 @@ import 'regenerator-runtime/runtime';
 
 describe('Home', () => {
   it('should be titled "Web Jam LLC"', async () => {
-    await page.goto('http://localhost:7000/', { waitUntil: 'load' });
+    await page.goto('http://localhost:7878/', { waitUntil: 'load' });
     await expect(page.title()).resolves.toMatch('Web Jam LLC');
   });
 });
