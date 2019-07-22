57b4e18c965c31465b88e984d94eda946dea2eb9
Rhayven <amayis@ymail.com>
2019-07-22 16:51:40 -0400

    Delete home.spec.js

--- test/puppeteer/home.spec.js
+++
@@ -1,9 +0,0 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
-describe('Home', () => {
-  it('should be titled "Web Jam LLC"', async () => {
-    await page.goto('http://localhost:7878/', { waitUntil: 'load' });
-    await expect(page.title()).resolves.toMatch('Web Jam LLC');
-  });
-});
