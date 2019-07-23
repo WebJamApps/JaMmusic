40a16e2929726326cd3afceccba6209714cc0c16
Rhayven <amayis@ymail.com>
2019-07-23 04:44:47 -0400

    linting

--- test/puppeteer/originals.spec.js
+++ test/puppeteer/originals.spec.js
@@ -1,11 +1,11 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 describe('Originals', () => {
   it('should be titled "Originals | Web Jam LLC"', async () => {
     const page = await browser.newPage();
     await page.goto('http://localhost:7878/music/originals', {
-      waitUntil: 'load'
+      waitUntil: 'load',
     });
     await expect(page.title()).resolves.toMatch('Originals | Web Jam LLC');
   });
