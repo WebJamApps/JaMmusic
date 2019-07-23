0360f2ac8bf11429e86e9672013c82b03c6f7235
Rhayven <amayis@ymail.com>
2019-07-23 04:38:53 -0400

    Fixed puppeteer tests

--- test/puppeteer/buymusic.spec.js
+++ test/puppeteer/buymusic.spec.js
@@ -3,6 +3,7 @@ import 'regenerator-runtime/runtime';
 
 describe('Buy Music', () => {
   it('should be titled "Buy Music | Web Jam LLC"', async () => {
+    const page = await browser.newPage();
     await page.goto('http://localhost:7878/music/buymusic', {
       waitUntil: 'load',
     });
