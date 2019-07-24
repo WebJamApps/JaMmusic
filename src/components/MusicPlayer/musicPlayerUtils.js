43575da69abc3b8b7237e20707e2c4262b4e2525
Rhayven <amayis@ymail.com>
2019-07-24 03:26:37 -0400

    WIP Unit tests

--- src/components/MusicPlayer/musicPlayerUtils.js
+++ src/components/MusicPlayer/musicPlayerUtils.js
@@ -37,13 +37,13 @@ const runIfOnePlayer = (controller) => {
   return null;
 };
 
-// eslint-disable-next-line consistent-return
 const resetSongs = (controller) => {
   const { songs: propSongs, copy } = controller.props;
   const { songs: stateSongs } = controller.state;
   if (propSongs.length !== stateSongs.length) {
     return controller.setState({ songs: propSongs, copy });
   }
+  return false;
 };
 
 const homeButton = onePlayerMode => (
