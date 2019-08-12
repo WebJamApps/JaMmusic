953627ca4590fa3f974b2a934aa03e14849c96e2
Joshua Sherman <jsherman@valcom.com>
2019-08-12 08:20:23 -0400

    unit test to check if songs is null

--- test/react-unit/App/authActions.spec.js
+++ test/react-unit/App/authActions.spec.js
@@ -9,36 +9,29 @@ const mockStore = configureMockStore(middlewares);
 describe('authActions', () => {
   it('authenticates', async () => {
     const store = mockStore({ auth: { isAuthenticated: false } });
-    let result;
-    try {
-      result = await store.dispatch(authenticate({ code: 'someCode' }));
-    } catch (e) { throw e; }
+    const result = await store.dispatch(authenticate({ code: 'someCode' }));
     expect(result.type).toBe('GOT_TOKEN');
   });
   it('does not fetch if already authenticated', async () => {
     const store = mockStore({ auth: { isAuthenticated: true } });
-    let result;
-    try { result = await store.dispatch(authenticate({ code: 'someCode' })); } catch (e) { throw e; }
+    const result = await store.dispatch(authenticate({ code: 'someCode' }));
     expect(result).toBe(true);
   });
   it('returns error when nothing is returned from Google', async () => {
     const store = mockStore({ auth: { isAuthenticated: false } });
-    let result;
     request.setMockResponse({ });
-    try { result = await store.dispatch(authenticate({ code: 'someCode' })); } catch (e) { throw e; }
+    const result = await store.dispatch(authenticate({ code: 'someCode' }));
     expect(result.type).toBe('AUTH_ERROR');
   });
   it('returns a fetch error', async () => {
     const store = mockStore({ auth: { isAuthenticated: false } });
-    let result;
     request.setMockError(new Error('bad'));
-    try { result = await store.dispatch(authenticate({ code: 'someCode' })); } catch (e) { throw e; }
+    const result = await store.dispatch(authenticate({ code: 'someCode' }));
     expect(result.type).toBe('AUTH_ERROR');
   });
   it('logs out the user', async () => {
     const store = mockStore({ auth: { isAuthenticated: true } });
-    let result;
-    try { result = await store.dispatch(logout()); } catch (e) { throw e; }
+    const result = await store.dispatch(logout());
     expect(result.type).toBe('LOGOUT');
   });
 });
--- test/react-unit/components/music-player.spec.js
+++ test/react-unit/components/music-player.spec.js
@@ -5,9 +5,6 @@ import songData from '../../../src/App/songs.json';
 
 function setup() {
   const { songs } = songData;
-  // const copy = Array.from(songData.songs.filter(song => song.category === 'originals'));
-
-  // const props = { songs, filterBy: 'originals' };
 
   window.document.body.innerHTML = '<div id="sidebar"></div><div id="header"/><div id="contentBlock"/>'
     + '<div id="wjfooter"/><div id="mobilemenutoggle"/><div id="pageContent"/><h4 id="headerTitle">'
@@ -24,40 +21,34 @@ describe('Music player component init', () => {
     const { wrapper } = setup();
     expect(wrapper.find('#mainPlayer').exists()).toBe(true);
   });
-
   it('should find and simulate play/pause', () => {
     const { wrapper } = setup();
     wrapper.find('button#play-pause').simulate('click');
     expect(wrapper.instance().state.player.playing).toBe(true);
   });
-
   it('should simulate the pause function', () => {
     const { wrapper } = setup();
     wrapper.instance().pause();
     expect(wrapper.instance().state.player.playing).toBe(false);
   });
-
   it('shuffles the songs', () => {
     const mp = new MusicPlayer({ songs: [{ _id: '123' }, { _id: '456' }], copy: [{ _id: '123' }, { _id: '456' }] });
     mp.state = { songs: [{ _id: '123' }, { _id: '456' }], copy: [{ _id: '123' }, { _id: '456' }], player: { isShuffleOn: false } };
     mp.setState = () => {};
     mp.shuffle();
   });
-
   it('should find and simulate stop shuffle and confirm shuffling is off', () => {
     const { wrapper } = setup();
     wrapper.instance().setState({ index: 100 });
     wrapper.instance().playEnd();
     expect(wrapper.instance().state.index).toBe(0);
   });
-
   it('should test the end of the player', () => {
     const { wrapper } = setup();
     wrapper.instance().setState({ player: { isShuffleOn: true } });
     wrapper.find('button#shuffle').simulate('click');
     expect(wrapper.instance().state.player.isShuffleOn).toBe(false);
   });
-
   it('advances to the next song', () => {
     const mp = new MusicPlayer({ songs: [{ _id: '123' }, { _id: '456' }], copy: [{ _id: '123' }, { _id: '456' }] });
     mp.state = {
@@ -66,20 +57,17 @@ describe('Music player component init', () => {
     mp.setState = () => {};
     mp.next();
   });
-
   it('should find and simulate a previous song function', () => {
     const { wrapper } = setup();
     wrapper.find('button#prev').simulate('click');
     expect(wrapper.instance().state.index).not.toBe(0);
   });
-
   it('should test previous song which has an index > 0', () => {
     const { wrapper } = setup();
     wrapper.instance().setState({ index: 1 });
     wrapper.instance().prev();
     expect(wrapper.instance().state.index).toBe(0);
   });
-
   it('should find and copy a playing song url', () => {
     const { wrapper } = setup();
     wrapper.instance().navigator = { clipboard: { async writeText(arg) { return arg; } } };
@@ -87,7 +75,6 @@ describe('Music player component init', () => {
     wrapper.find('#copyButton').simulate('click');
     expect(wrapper.instance().state.player.displayCopier).toBe('none');
   });
-
   it('should hide copier message after showing for 1.5s', (done) => {
     const { wrapper } = setup();
     wrapper.instance().navigator = { clipboard: { async writeText(arg) { return arg; } } };
@@ -96,47 +83,23 @@ describe('Music player component init', () => {
     expect(wrapper.instance().state.player.displayCopier).toBe('none');
     setTimeout(() => done(), 1501);
   });
-
   it('should toggle off copying/share pane', () => {
     const { wrapper } = setup();
     wrapper.instance().setState({ player: { displayCopier: 'block' } });
     wrapper.instance().share();
     expect(wrapper.instance().state.player.displayCopier).toBe('none');
   });
-
   it('should toggle on copying/share pane', () => {
     const { wrapper } = setup();
     wrapper.instance().setState({ player: { displayCopier: 'none' } });
     wrapper.instance().share();
     expect(wrapper.instance().state.player.displayCopier).toBe('block');
   });
-
-  // it('should test props passed to Music Player', () => {
-  //   const { wrapper, props } = setup();
-  //   expect(wrapper.props().songs).toEqual(props.copy);
-  // });
-
-  // it('should test one player mode', () => {
-  //   const { location } = window;
-  //   delete window.location;
-  //
-  //   window.location = {
-  //     search: '?oneplayer=true&id=28ru9weis2309urihw9098ewuis',
-  //     pathname: '/music/original',
-  //     href: '/music',
-  //   };
-  //
-  //   const { wrapper } = setup();
-  //   wrapper.update();
-  //
-  //   // restore window
-  //   window.location = location;
-  //   expect(wrapper.instance().state.player.onePlayerMode).toBeTruthy();
-  // });
-
-  // it('finish off one-player mode to home', () => {
-  //   const { wrapper } = setup();
-  //   wrapper.find('button#h').simulate('click');
-  //   expect(window.location.href).toBe(undefined);
-  // });
+  it('returns a null playUrl', (done) => {
+    const { wrapper } = setup();
+    wrapper.instance().setState({ song: null });
+    const result = wrapper.instance().playUrl();
+    expect(result).toBe(null);
+    done();
+  });
 });
