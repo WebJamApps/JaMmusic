db7dc2f0e8c2e1095ac7d587142fa9bd934fb493
Joshua Sherman <jsherman@valcom.com>
2019-08-14 19:12:12 -0400

    fixes mission and pub buttons

--- src/containers/Originals/index.js
+++ src/containers/Originals/index.js
@@ -12,41 +12,6 @@ export class Originals extends Component {
 
   componentDidMount() { document.title = 'Originals | Web Jam LLC'; }
 
-  // // eslint-disable-next-line class-methods-use-this
-  // setIndex(songs, category) {
-  //   let categorySongs = [];
-  //   const otherSongs = [];
-  //   for (let i = 0; songs.length > i; i += 1) {
-  //     // eslint-disable-next-line security/detect-object-injection
-  //     if (songs[i].category === category) {
-  //       // eslint-disable-next-line security/detect-object-injection
-  //       categorySongs.push(songs[i]);
-  //     } else {
-  //       // eslint-disable-next-line security/detect-object-injection
-  //       otherSongs.push(songs[i]);
-  //     }
-  //   }
-  //
-  //   categorySongs = categorySongs.concat(otherSongs);
-  //   return categorySongs;
-  // }
-
-  // ToggleSongTypes(type) {
-  //   return () => {
-  //     let { songs } = this.state;
-  //     const { allSongs } = this.state;
-  //     const typeInState = `${type}State`;
-  //     const typeState = this.state[typeInState.toString()]; // eslint-disable-line react/destructuring-assignment
-  //     if (typeState === 'off') {
-  //       songs = [...songs, ...allSongs.filter(song => song.category === type)];
-  //     } else {
-  //       songs = songs.filter(song => song.category !== type);
-  //     }
-  //     songs = this.setIndex(songs, type);
-  //     this.setState({ songs, [typeInState]: typeState === 'off' ? 'on' : 'off' });
-  //   };
-  // }
-
   render() {
     const { songs } = this.props;
     return (
@@ -65,10 +30,6 @@ export class Originals extends Component {
               ? (
                 <div id="playerAndButtons">
                   <DefaultMusicPlayer filterBy="originals" />
-                  {/* <div id="mAndP" style={{ height: '22px', margin: 'auto' }}>
-                    <button type="button" onClick={this.ToggleSongTypes('mission')} className={`mission ${missionState}`}> Mission </button>
-                    <button type="button" onClick={this.ToggleSongTypes('pub')} className={`pub ${pubState}`}> Pub </button>
-                  </div> */}
                 </div>
               )
               : null
