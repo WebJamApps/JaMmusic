4f9a34b6dd5eb0a4061ba1955f1a36e13a1eb8bf
Joshua V Sherman <joshua.v.sherman@gmail.com>
2019-09-03 17:25:32 -0400

    Update intro.js

--- src/containers/Music/intro.js
+++ src/containers/Music/intro.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Intro = () => (
   <div className="intro">
     <p style={{ marginTop: '10px' }}>
-      Josh and Maria have been performing their music together for over six years now!
+      Josh and Maria have been performing their music together for over eight years now!
       Whether it is at church, charity events, public venues, or outdoor festivals, this couple will blow your socks off.
       Click&nbsp;
       <Link to="/music/originals">here</Link>
