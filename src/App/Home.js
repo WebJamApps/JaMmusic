57cccca240ad8461c5aebd295da7c4462f0ee8fc
Rhayven <codingforllamas@gmail.com>
2019-09-06 00:21:14 -0400

    Modified home to change depending on size and clean installed

--- src/App/Home.js
+++ src/App/Home.js
@@ -1,5 +1,6 @@
 /* eslint-disable consistent-return */
 import React, { Component } from 'react';
+import ReactResizeDetector from 'react-resize-detector';
 import WideAboutUs from './Homepage/Widescreen/WideAbout';
 import WideCurrentProjects from './Homepage/Widescreen/WideCurrentProjects';
 import WideTicker from './Homepage/Widescreen/WideFacebookTicker';
@@ -9,30 +10,47 @@ import NarrowTicker from './Homepage/Narrowscreen/NarrowFacebookTicker';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.parentRef = React.createRef();
+    this.onResize = this.onResize.bind(this);
+    this.state = { width: 100 };
+  }
+
+  onResize(width) {
+    this.setState({ width });
+  }
+
   render() {
+    const { width } = this.state;
     return (
     // Add link-to-top for accessibility
       <div>
-        <div className="page-content">
-          <div className="widescreenHomepage">
-            <WideAboutUs />
-            <hr />
-            <WideCurrentProjects />
-          </div>
-          <div className="notWidescreen">
-            <NarrowAboutUs />
-            <hr />
-            <NarrowCurrentProjects />
-          </div>
-        </div>
-
-        <p style={{ fontSize: '6pt', marginBottom: '0' }}>&nbsp;</p>
+        {width > 1008
+          ? (
+            <div className="page-content">
+              <WideAboutUs />
+              <hr />
+              <WideCurrentProjects />
+              <WideTicker />
+            </div>
+          )
+          : (
+            <div className="page-content">
+              <NarrowAboutUs />
+              <hr />
+              <NarrowCurrentProjects />
+              <NarrowTicker />
+            </div>
+          )}
+        {/* <p style={{ fontSize: '6pt', marginBottom: '0' }}>&nbsp;</p>
         <div className="widescreenHomepage">
           <WideTicker />
         </div>
         <div className="notWidescreen">
           <NarrowTicker />
-        </div>
+        </div> */}
+        <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} targetDomEl={this.parentRef.current} />
       </div>
     );
   }
