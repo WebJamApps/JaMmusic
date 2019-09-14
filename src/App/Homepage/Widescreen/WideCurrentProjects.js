d527f7a6caa100e849db127976551da90ab72ab4
Rhayven <codingforllamas@gmail.com>
2019-09-14 03:55:38 -0400

    More styling refactoring

--- src/App/Homepage/Widescreen/WideCurrentProjects.js
+++ src/App/Homepage/Widescreen/WideCurrentProjects.js
@@ -8,27 +8,24 @@ const WideCurrentProjects = () => (
   <div className="widescreenHomepage">
     <div className="material-content">
       <div className="container">
-        <h3 style={{ textAlign: 'center', marginTop: '15px', marginBottom: '15px' }}>Our Current Projects</h3>
+        <h3>Our Current Projects</h3>
         <div className="row">
           <div className="col">
-
             <CollegeLutheran />
-
           </div>
-
-          <p style={{ fontSize: '6pt', marginBottom: '0' }}>&nbsp;</p>
-
+          <p className="spacer">&nbsp;</p>
           <div className="col">
             <AppersonAutomotive />
           </div>
         </div>
 
-        <p style={{ fontSize: '6pt', marginBottom: '0' }}>&nbsp;</p>
+        <p className="spacer">&nbsp;</p>
+
         <div className="row">
           <div className="col">
             <OHAF />
           </div>
-          <p style={{ fontSize: '6pt', marginBottom: '0' }}>&nbsp;</p>
+          <p className="spacer">&nbsp;</p>
           <div className="col">
             <TroPeaks />
           </div>
