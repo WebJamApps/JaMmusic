e2ed3d68080d6761fce89c101e588776bd5d84e0
Joshua Sherman <jsherman@valcom.com>
2019-09-30 08:39:42 -0400

    unit tests

--- test/react-unit/App/menuUtils.spec.js
+++ test/react-unit/App/menuUtils.spec.js
@@ -1,12 +1,37 @@
-import authUtils from '../../../src/App/authUtils';
+import menuUtils from '../../../src/App/menuUtils';
 
 describe('menuUtils', () => {
   const controllerStub = {
-    props: { auth: { token: 'token' }, dispatch: () => Promise.resolve(true) },
+    googleButtons: () => true,
+    makeMenuLink: () => true,
+    props: {
+      location: { pathname: '/music' },
+      auth: { token: 'token', isAuthenticated: true, user: { userType: 'Developer' } },
+      dispatch: () => Promise.resolve(true),
+    },
   };
-  it('handles menuItem', (done) => {
-    const result = authUtils.responseGoogleFailLogin('no way');
-    expect(result).toBe(false);
+  it('handles menuItem for Develper', (done) => {
+    const result = menuUtils.menuItem({ link: '/music', type: 'link', auth: true },
+      1, controllerStub);
+    expect(result).toBe(true);
+    done();
+  });
+  it('handles menuItem for Shop', (done) => {
+    const result = menuUtils.continueMenuItem({ link: '/shop', type: 'link', auth: true },
+      1, { pathname: '/shop' }, {}, controllerStub);
+    expect(result).toBe(true);
+    done();
+  });
+  it('handles menuItem for Shop when at root', (done) => {
+    const result = menuUtils.continueMenuItem({ link: '/', type: 'link', auth: true },
+      1, { pathname: '/shop' }, {}, controllerStub);
+    expect(result).toBe(true);
+    done();
+  });
+  it('handles menuItem for GoogleLogout', (done) => {
+    const result = menuUtils.continueMenuItem({ link: '/', type: 'googleLogout', auth: true },
+      1, { pathname: '/music' }, { isAuthenticated: true }, controllerStub);
+    expect(result).toBe(true);
     done();
   });
 });
