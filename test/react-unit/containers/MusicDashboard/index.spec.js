1e97b23708851b11338330b90686ce02bc529ca1
Rhayven <codingforllamas@gmail.com>
2019-10-10 01:33:12 -0400

    WIP Updating tests

--- test/react-unit/containers/MusicDashboard/index.spec.js
+++ test/react-unit/containers/MusicDashboard/index.spec.js
@@ -6,12 +6,20 @@ describe('Dashboard Container', () => {
   let wrapper;
   beforeEach(() => {
     const auth = { token: '' };
-    wrapper = shallow(<MusicDashboard auth={auth} />);
+    wrapper = shallow(<MusicDashboard auth={auth} scc={{ emit: () => {} }} />);
   });
   it('is defined', () => {
     expect(MusicDashboard).toBeDefined();
   });
   it('renders correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+  it('calls on change', () => {
+    wrapper.instance().setState = jest.fn((boobyJ) => { expect(boobyJ.hi).toBe(11); });
+    wrapper.instance().onChange({ preventDefault: () => {}, target: { id: 'hi', value: 11 } });
+  });
+  it('calls the tour API', () => {
+    wrapper.instance().setState = jest.fn((obJ) => { expect(obJ.redirect).toBe(true); });
+    wrapper.instance().createTourApi({ date: '2019-10-10' });
   });
 });
