b7ce01cdbd00285701336ec70f3bdf03cfa28b90
Rhayven <codingforllamas@gmail.com>
2019-10-12 06:57:59 -0400

    WIP Test Updates
    Uploading for code review/help

--- test/react-unit/containers/MusicDashboard/index.spec.js
+++ test/react-unit/containers/MusicDashboard/index.spec.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { MemoryRedirect } from 'react-router-dom';
 import { MusicDashboard } from '../../../../src/containers/MusicDashboard';
 
 describe('Dashboard Container', () => {
   let wrapper;
+  // const controller = {
+  //   state: {
+  //     redirect: false,
+  //     date: '10-10-2019',
+  //     time: '11:11',
+  //     tickets: '1',
+  //     more: 'yes',
+  //   },
+  //   props: {
+  //     location: { pathname: '/music/dashboard' },
+  //   },
+  // };
   beforeEach(() => {
     const auth = { token: '' };
     wrapper = shallow(<MusicDashboard auth={auth} scc={{ emit: () => {} }} />);
@@ -21,5 +34,10 @@ describe('Dashboard Container', () => {
   it('calls the tour API', () => {
     wrapper.instance().setState = jest.fn((obJ) => { expect(obJ.redirect).toBe(true); });
     wrapper.instance().createTourApi({ date: '2019-10-10' });
+  });
+  it('redirects to /music', (done) => {
+    const result = wrapper.instance(<MemoryRedirect to="/music" />);
+    expect(result).toBe(true);
+    done();
   });
 });
