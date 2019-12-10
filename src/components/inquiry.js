ea63b2098c49d7e22cd04f1e3ecdf3898456878a
Joshua Sherman <jsherman@valcom.com>
2019-12-10 09:56:14 -0500

    form validation

--- src/components/inquiry.js
+++ src/components/inquiry.js
@@ -8,7 +8,7 @@ export default class inquiry extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      submitted: false, comments: '', uSAstate: '', country: 'Afghanistan', zipcode: '', phonenumber: '', emailaddress: '', fullname: '',
+      submitted: false, comments: '', uSAstate: '--', country: '--', zipcode: '', phonenumber: '', emailaddress: '', fullname: '', formError: '',
     };
     this.stateValues = stateData;
     this.forms = forms;
@@ -36,21 +36,31 @@ export default class inquiry extends Component {
 
   validateForm() {
     const {
-      fullname, emailaddress, comments, zipcode, phonenumber,
+      fullname, emailaddress, comments, zipcode, phonenumber, formError, country, uSAstate,
     } = this.state;
-    let validEmail = false,
-      validPhoneNumber = false;
+    let validEmail = false, validPhoneNumber = false, validState = false;
     // eslint-disable-next-line no-useless-escape
     const regEx = RegExp('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$');
-    const phoneno = /^[0-9]{10,20}$/;
+    const phoneno = /^\d{10}$/;
     if (regEx.test(emailaddress) && emailaddress.includes('.')) {
       validEmail = true;
+      if (formError === 'Invalid email format') this.setState({ formError: '' });
+    } else {
+      if (formError === '') this.setState({ formError: 'Invalid email format' });
+      return true;
     }
-    if (phoneno.test(phonenumber)) {
-      validPhoneNumber = true;
+    if (phoneno.test(phonenumber)) validPhoneNumber = true;
+    if (phonenumber !== '' && !validPhoneNumber) {
+      if (formError !== 'Ten-digit phone number') this.setState({ formError: 'Ten-digit phone number' });
+      return true;
     }
-    if (phonenumber !== '' && !validPhoneNumber) return true;
-    if (fullname && emailaddress && zipcode && comments !== '' && validEmail) return false;
+    if (country === 'United States' && uSAstate !== '--')validState = true;
+    if (country !== 'United States')validState = true;
+    if (fullname && emailaddress && zipcode && comments !== '' && validEmail && country !== '--' && validState) {
+      if (formError !== '') this.setState({ formError: '' });
+      return false;
+    }
+    if (formError === '' || formError === 'Ten-digit phone number') this.setState({ formError: 'Complete missing form fields' });
     return true;
   }
 
@@ -78,7 +88,7 @@ export default class inquiry extends Component {
 
   countryDropdown(country) {
     return (
-      <label htmlFor="country">
+      <label htmlFor="country" style={{ paddingTop: '12px' }}>
           * Country
         <br />
         <select value={country} onChange={(event) => this.handleChange(event, true)}>
@@ -92,10 +102,9 @@ export default class inquiry extends Component {
 
   statesDropdown(uSAstate) {
     return (
-      <label htmlFor="state">
-        <span style={{ display: 'table', padding: '1px 8px' }}>
-          State
-        </span>
+      <label htmlFor="state" style={{ paddingTop: '12px' }}>
+          * State
+        <br />
         <select value={uSAstate} onChange={(evt) => this.onChange(evt, true)}>
           {
             this.stateValues.map((sv) => <option id={sv} key={sv} value={sv}>{sv}</option>)
@@ -106,12 +115,11 @@ export default class inquiry extends Component {
   }
 
   newContactForm(fullname, emailaddress, phonenumber, zipcode, comments, buttonStyle) {
-    const { country } = this.state;
+    const { country, formError } = this.state;
     return (
-      <form id="new-contact" style={{ marginTop: '4px', maxWidth: '90%' }}>
+      <form id="new-contact" style={{ maxWidth: '316px', marginLeft: '10px' }}>
         {this.forms.makeInput('text', 'Full Name', true, this.onChange, fullname)}
         {this.forms.makeInput('email', 'Email Address', true, this.onChange, emailaddress)}
-        { this.forms.makeInput('zip', 'Zipcode', true, this.onChange, zipcode)}
         { this.forms.makeInput('tel', 'Phone Number (Digits Only)', false, this.onChange, phonenumber)}
         { this.countryDropdown() }
         { country === 'United States'
@@ -119,12 +127,14 @@ export default class inquiry extends Component {
             this.statesDropdown()
           )
           : null}
+        { this.forms.makeInput('zip', 'Zipcode', true, this.onChange, zipcode)}
         <label htmlFor="comments">
           * Comments
           <br />
           <textarea style={{ minWidth: '3in', paddingLeft: '5px' }} value={comments} onChange={this.handleChange} />
         </label>
-        <div className="inquiryValidation">
+        <p className="form-errors" style={{ color: 'red' }}>{formError}</p>
+        <div className="inquiryValidation" style={{ marginBottom: '12px' }}>
           <span className="inquiryValidation">* Required</span>
           <button style={buttonStyle} disabled={this.validateForm()} type="button" onClick={this.createEmail}>Send</button>
         </div>
@@ -137,17 +147,16 @@ export default class inquiry extends Component {
       submitted, fullname, emailaddress, comments, phonenumber, zipcode, buttonStyle,
     } = this.state;
     return (
-      <div style={{ border: '1px solid black', maxWidth: '4in', margin: 'auto' }}>
+      <div style={{ maxWidth: '320px', margin: 'auto', border: '1px solid black' }}>
         { submitted === false ? (
-          <div className="page-content">
+          <div className="contact-form">
             <h4 style={{
-              textAlign: 'center', margin: '14px', marginTop: 0, paddingTop: 0, fontWeight: 'bold',
+              textAlign: 'center', marginBottom: '0', marginTop: '10px', paddingTop: 0, fontWeight: 'bold',
             }}
             >
               Contact Us
             </h4>
             {this.newContactForm(fullname, emailaddress, phonenumber, zipcode, comments, buttonStyle)}
-            <p>&nbsp;</p>
           </div>
         ) : (
           <div className="page-content contacted">
