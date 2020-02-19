3091bba67b15c20fffc41dc31d2acbb30167e1e7
Joshua Sherman <jsherman@valcom.com>
2020-02-19 18:24:57 -0500

    connects and sends new websocket message

--- src/App/connectToSC.js
+++ src/App/connectToSC.js
@@ -1,24 +1,33 @@
 import socketCluster from 'socketcluster-client';
+import scc from 'scc';
 
 const setupSocketCluster = (dispatch) => {
-  const scc = socketCluster.create({
+  const sccOld = socketCluster.create({
     hostname: process.env.SOCKETCLUSTER_HOST,
     port: process.env.SOCKETCLUSTER_PORT,
     autoConnect: true,
     secure: process.env.SOCKETCLUSTER_SECURE !== 'false',
   });
-  scc.on('connect', () => {
-    const count = scc.subscribe('sample');// eslint-disable-next-line security/detect-non-literal-fs-filename
+  sccOld.on('connect', () => {
+    const count = sccOld.subscribe('sample');// eslint-disable-next-line security/detect-non-literal-fs-filename
     count.watch((numbUsers) => dispatch({ type: 'NUM_USERS', numbUsers }));
-    const newTour = scc.subscribe('tourCreated');// eslint-disable-next-line security/detect-non-literal-fs-filename
+    const newTour = sccOld.subscribe('tourCreated');// eslint-disable-next-line security/detect-non-literal-fs-filename
     newTour.watch((data) => dispatch({ type: 'NEW_TOUR', data }));
     console.log(`socketClusterClient connected on port ${process.env.SOCKETCLUSTER_PORT}`);// eslint-disable-line no-console
-    scc.emit('sampleClientEvent', 'howdy');
-    scc.emit('getTours');
+    sccOld.emit('sampleClientEvent', 'howdy');
+    sccOld.emit('getTours');
   });
-  scc.on('random', (data) => dispatch({ type: 'SC_HEARTBEAT', data }));
-  scc.on('allTours', (data) => dispatch({ type: 'ALL_TOUR', data }));
-  dispatch({ type: 'SCC', scc });
+  sccOld.on('random', (data) => dispatch({ type: 'SC_HEARTBEAT', data }));
+  sccOld.on('allTours', (data) => dispatch({ type: 'ALL_TOUR', data }));
+  dispatch({ type: 'SCC', sccOld });
   return Promise.resolve(true);
 };
-export default { setupSocketCluster }; // emits when a style was updated or created
+const connectToSCC = () => {
+  const socket = scc.create({
+    hostname: 'localhost',
+    port: 8888,
+  });
+  socket.transmit('howdy', 123);
+  return Promise.resolve(true);
+};
+export default { setupSocketCluster, connectToSCC }; // emits when a style was updated or created
