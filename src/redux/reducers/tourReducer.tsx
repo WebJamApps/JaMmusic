71cd98bf84b96af095556b4a2a7ff4be34f5d0f4
JoshuaVSherman <joshua.v.sherman@gmail.com>
2020-04-03 09:47:03 -0400

    WIP edit tour

--- src/redux/reducers/tourReducer.tsx
+++ src/redux/reducers/tourReducer.tsx
@@ -1,21 +1,23 @@
-// @ts-nocheck
 const initialState = {
   tour: [],
   tourUpdated: false,
+  editTour: {},
 };
 
-const addTour = (state, action) => {
+const addTour = (state: {tour: {datetime: string}[]}, action: {data?: {datetime: string}}) => {
   const tArr = state.tour;
   tArr.push(action.data);
+  // @ts-ignore
   tArr.sort((a, b) => new Date(b.datetime) - new Date(a.datetime));
   return { ...state, tour: tArr, tourUpdated: true };
 };
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action: {type: string; data?: {datetime: string; _id: string}}) => {
   switch (action.type) {
     case 'NEW_TOUR': return addTour(state, action);
     case 'ALL_TOUR': return { ...state, tour: action.data, tourUpdated: false };
     case 'RESET_TOUR': return { ...state, tourUpdated: false };
+    case 'EDIT_TOUR': return { ...state, editTour: action.data };
     default: return state;
   }
 };
---
+++ test/redux/tourReducer.spec.tsx
@@ -0,0 +1,42 @@
+import reducer from '../../src/redux/reducers/tourReducer';
+
+describe('tour reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, { type: '', data: { datetime: '', _id: '' } })).toEqual(
+      {
+        tour: [],
+        tourUpdated: false,
+        editTour: {},
+      },
+    );
+  });
+  it('handles RESET_TOUR', () => {
+    expect(reducer(undefined, { type: 'RESET_TOUR' })).toEqual(
+      {
+        tour: [],
+        tourUpdated: false,
+        editTour: {},
+      },
+    );
+  });
+  it('handles NEW_TOUR', () => {
+    const t1 = { datetime: `${new Date().toISOString}`, _id: '' };
+    const t2 = { datetime: `${new Date().toISOString}`, _id: '' };
+    expect(reducer({ tour: [t1], tourUpdated: true, editTour: {} }, { type: 'NEW_TOUR', data: t2 })).toEqual(
+      {
+        tour: [t2, t1],
+        tourUpdated: true,
+        editTour: {},
+      },
+    );
+  });
+  it('handles EDIT_TOUR', () => {
+    expect(reducer(undefined, { type: 'EDIT_TOUR', data: { _id: '123', datetime: 'string' } })).toEqual(
+      {
+        tour: [],
+        tourUpdated: false,
+        editTour: { _id: '123', datetime: 'string' },
+      },
+    );
+  });
+});
