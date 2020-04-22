76e68b47394bdfaec77b7ccf08d59297e9d091b5
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-04-22 17:39:24 -0400

    more refactoring to typescript

---
+++ test/App/fof.spec.tsx
@@ -0,0 +1,17 @@
+ 
+import React from 'react';
+import { shallow } from 'enzyme';
+import FourOhFour from '../../src/App/404';
+
+function setup() {
+  const props = {};
+  const wrapper = shallow(<FourOhFour />);
+  return { wrapper, props };
+}
+
+describe('/404', () => {
+  it('renders the component', () => {
+    const { wrapper } = setup();
+    expect(wrapper.find('div.page-content').exists()).toBe(true);
+  });
+});
