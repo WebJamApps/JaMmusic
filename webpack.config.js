c5be5f2926e7f075d15842db9cdf488353d0b15d
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-04-25 13:23:31 -0400

    reinstall and eslint config tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -106,7 +106,6 @@ module.exports = ({
         use: scssRules,
       },
       { test: /\.html$/i, loader: 'html-loader' }, // eslint-disable-next-line no-useless-escape
-      { test: /[\/\\]node_modules[\/\\]bluebird[\/\\].+\.js$/, loader: 'expose-loader?Promise' },
       // embed small images and fonts as Data Urls and larger ones as files:
       { test: /\.(png|gif|jpg|cur)$/i, loader: 'url-loader', options: { limit: 8192 } },
       { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff2' } },
