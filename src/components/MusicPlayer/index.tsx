bd61f1ec23135643822d367a752d95f37bdb927e
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-06-08 09:01:05 -0400

    security fix and wording

--- src/components/MusicPlayer/index.tsx
+++ src/components/MusicPlayer/index.tsx
@@ -134,6 +134,8 @@ export class MusicPlayer extends Component<{ songs: any; filterBy: any }, MusicP
     let { song } = this.state, composer = '';
     if (!song) song = {};
     if (song.composer !== undefined && !song.composer.includes('Josh')) composer = ` by ${song.composer}`;
+    let quote = `Click the graphic below to hear ${song.artist} performing the song, "${song.title}"${composer}`;
+    if(song.category==='original')quote = quote.replace('performing the song', 'performing their song');
     return (
       <div id="share-buttons" style={{ display: 'inline-block' }}>
         <button type="button" id="share-button" role="menu" onClick={() => this.musicPlayerUtils.share(this)}>Share</button>
@@ -143,7 +145,7 @@ export class MusicPlayer extends Component<{ songs: any; filterBy: any }, MusicP
             backgroundColor: 'white', marginLeft: 0, paddingLeft: '5px', marginBottom: 0,
           }}
           url={url}
-          quote={`Click the graphic below to hear ${song.artist} performing the song, "${song.title}"${composer}`}
+          quote={quote}
           hashtag="#JoshAndMariaMusic"
         >
           <FacebookIcon round size={26} />
