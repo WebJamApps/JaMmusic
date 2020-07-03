ad01433ef3c7ec5add925bc6c35fafb0e4e69dde
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-07-03 17:07:27 -0400

    does not display the map yet

--- src/containers/GoogleMap/index.tsx
+++ src/containers/GoogleMap/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import UserMap from './UserMap';
 import CompanyMap from './CompanyMap';
 
 const GoogleMap = (): JSX.Element => {
+  useEffect(() => {
+    const gMap = new google.maps.Map(document.getElementById('googleMap'), { zoom: 2, center: { lat: 0, lng: 0 } });
+    console.log(gMap);
+  });
   const userMap = new UserMap();
   console.log(userMap);
   const companyMap = new CompanyMap();
@@ -11,6 +15,7 @@ const GoogleMap = (): JSX.Element => {
     <div className="page-content">
       <div style={{ margin: 'auto', textAlign: 'center' }}>
         <h2 style={{ marginTop: '10px' }}>Google Map Here</h2>
+        <div id="googleMap" />
       </div>
     </div>
   );
