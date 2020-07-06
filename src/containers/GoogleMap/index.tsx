47962c46a229cb5aa20bfa66df9a1e11a8b1f6a5
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-07-06 08:12:11 -0400

    google map on page

--- src/containers/GoogleMap/index.tsx
+++ src/containers/GoogleMap/index.tsx
@@ -1,23 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import UserMap from './UserMap';
 import CompanyMap from './CompanyMap';
 
-const GoogleMap = (): JSX.Element => {
-  useEffect(() => {
-    const gMap = new google.maps.Map(document.getElementById('googleMap'), { zoom: 2, center: { lat: 0, lng: 0 } });
+class GoogleMap extends React.Component {
+  componentDidMount() {
+    const gMap = new google.maps.Map(document.getElementById('googleMap'), { zoom: 3, center: { lat: 40, lng: -100 } });
+    console.log('did mount');
     console.log(gMap);
-  });
-  const userMap = new UserMap();
-  console.log(userMap);
-  const companyMap = new CompanyMap();
-  console.log(companyMap);
-  return (
-    <div className="page-content">
-      <div style={{ margin: 'auto', textAlign: 'center' }}>
-        <h2 style={{ marginTop: '10px' }}>Google Map Here</h2>
-        <div id="googleMap" />
+  }
+
+  render() {
+    const userMap = new UserMap();
+    console.log(userMap);
+    const companyMap = new CompanyMap();
+    console.log(companyMap);
+    return (
+      <div className="page-content">
+        <div style={{ margin: 'auto', textAlign: 'center' }}>
+          <h2 style={{ marginTop: '10px' }}>Google Map Here</h2>
+          <div id="googleMap" style={{ height: '4in' }} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default GoogleMap;
