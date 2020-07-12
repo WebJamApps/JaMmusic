62a976285c4fe8719cb13b57272081026fe4a1ed
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-07-12 19:59:07 -0400

    sorting examples

--- src/containers/SortContainer/index.tsx
+++ src/containers/SortContainer/index.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 export function SortContainer(): JSX.Element {
   const dice = Array.from({ length: 6 }, () => Math.floor(Math.random() * 6));
   const sorted = [...dice].sort();
+  const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const withNoDigits = randomString.replace(/[0-9]/g, '');
   return (
     <div
       id="sort-container"
@@ -12,14 +14,23 @@ export function SortContainer(): JSX.Element {
       }}
     >
       <h4>Typescript Sorting Example</h4>
-      <p style={{ textAlign: 'left', paddingLeft: '20px' }}>
-        Given this array:
+      <p style={{ textAlign: 'left', marginLeft: '10px' }}>
+        Given this random number array:
         {' '}
         <strong>{dice}</strong>
         <br />
         This is the sorted array:
         {' '}
         <strong>{sorted}</strong>
+        <br />
+        <br />
+        Given this random string of letters:
+        {' '}
+        <strong>{withNoDigits}</strong>
+        <br />
+        This is the sorted string:
+        {' '}
+        <strong>{withNoDigits.split('').sort().join('')}</strong>
       </p>
     </div>
   );
