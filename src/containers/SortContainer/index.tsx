fff0f1768228883b08c5997c3ba406d1d07505ac
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-07-13 14:01:09 -0400

    sort

--- src/containers/SortContainer/index.tsx
+++ src/containers/SortContainer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function makeRandomString(): string {
   const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  const withNoDigits = randomString.replace(/[0-9]/g, '');
+  const withNoDigits = randomString.replace(/[0-9]/g, 'X');
   return withNoDigits;
 }
 export function SortContainer(): JSX.Element {
@@ -11,7 +11,7 @@ export function SortContainer(): JSX.Element {
   const randomString = makeRandomString();
   return (
     <div id="sort-container" style={{ margin: 'auto', textAlign: 'center' }}>
-      <h4>Typescript Sorting Example</h4>
+      <h4>Sorting Example</h4>
       <p style={{ textAlign: 'left', marginLeft: '10px' }}>
         Given this random number array:
         {' '}
@@ -28,7 +28,9 @@ export function SortContainer(): JSX.Element {
         <br />
         This is the sorted string:
         {' '}
-        <strong>{randomString.split('').sort().join('')}</strong>
+        <strong>
+          {randomString.split('').sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' })).join('')}
+        </strong>
       </p>
     </div>
   );
