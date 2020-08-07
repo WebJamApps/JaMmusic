933d08157eac44b5ff9e619d4879fd31aa7990c4
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-08-07 10:02:47 -0400

    fix unit test

--- test/components/Inquiry/Inquiry.spec.tsx
+++ test/components/Inquiry/Inquiry.spec.tsx
@@ -20,8 +20,10 @@ describe('Inquiry Form', () => {
     expect(wrapper.instance().state.uSAstate).toBe('Alaska');
   });
   it('calls on change for text input', () => {
-    wrapper.instance().setState = jest.fn((obj) => { expect(obj.hi).toBe('11'); });
-    wrapper.instance().onChange({ target: { id: 'hi', value: '11 ' } });
+    wrapper.instance().setState = jest.fn();
+    const evt:any = { target: { id: 'hi', value: '11 ' } };
+    wrapper.instance().onInputChange(evt);
+    expect(wrapper.instance().setState).toHaveBeenCalled();
   });
   it('calls on change for uSAstate dropdown', () => {
     wrapper.instance().setState = jest.fn((obj) => { expect(obj.uSAstate).toBe('Alaska'); });
