30361696b5d84c0375c0bb3c28d98b94025370c7
Rhayven <codingforllamas@gmail.com>
2020-08-14 22:48:50 -0400

    WIP Fixes src/redux any's

--- src/redux/reducers/authReducer.ts
+++ src/redux/reducers/authReducer.ts
@@ -5,17 +5,22 @@ const initialState = {
   token: '',
   user: {},
 };
-const reducer = (state = initialState, action: any): any => {
+const reducer = (state = initialState,
+  action: { type: string; data?: { name?: string, email?: string; token?: string; }; error?: { message?: string; }; }): Record<string, unknown> => {
   switch (action.type) {
     case 'GOT_TOKEN':
+      // eslint-disable-next-line no-param-reassign
+      if (!action.data)action.data = {};
       return {
-        ...state, isAuthenticated: true, email: action.data.email, token: action.data.token, error: '',
+        ...state, isAuthenticated: true, email: action.data.email || '', token: action.data.token || '', error: '',
       };
     case 'LOGOUT':
       return {
         ...state, isAuthenticated: false, email: '', token: '', error: '', user: {},
       };
     case 'AUTH_ERROR':
+      // eslint-disable-next-line no-param-reassign
+      if (!action.error)action.error = {};
       return {
         ...state, isAuthenticated: false, email: '', token: '', error: action.error.message, user: {},
       };
--- test/redux/authReducer.spec.ts
+++ test/redux/authReducer.spec.ts
@@ -1,59 +1,55 @@
 import reducer from '../../src/redux/reducers/authReducer';
 
 describe('auth reducer', () => {
-  it('returns the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(
-      {
-        isAuthenticated: false,
-        error: '',
-        email: '',
-        token: '',
-        user: {},
-      },
-    );
-  });
-  it('handles GOT_TOKEN', () => {
-    expect(
-      reducer(undefined, { type: 'GOT_TOKEN', data: { email: 'j@b.com', token: '123' } }),
-    ).toEqual(
-      {
-        isAuthenticated: true,
-        error: '',
-        email: 'j@b.com',
-        token: '123',
-        user: {},
-      },
-    );
-  });
-  it('handles LOGOUT', () => {
-    expect(
-      reducer(undefined, { type: 'LOGOUT' }),
-    ).toEqual(
-      {
-        isAuthenticated: false,
-        error: '',
-        email: '',
-        token: '',
-        user: {},
-      },
-    );
-  });
-  it('handles AUTH_ERROR', () => {
-    expect(
-      reducer(undefined, {
-        type: 'AUTH_ERROR',
-        error: { message: 'bad' },
-      }),
-    ).toEqual(
-      {
-        isAuthenticated: false,
-        error: 'bad',
-        email: '',
-        token: '',
-        user: {},
-      },
-    );
-  });
+  it('returns the initial state', () => expect(reducer(undefined, {
+    type: '',
+    data: { name: '', email: '', token: '' },
+    error: { message: '' },
+  })).toEqual(
+    {
+      isAuthenticated: false,
+      error: '',
+      email: '',
+      token: '',
+      user: {},
+    },
+  ));
+  it('handles GOT_TOKEN', () => expect(
+    reducer(undefined, { type: 'GOT_TOKEN', data: { email: 'j@b.com', token: '123' } }),
+  ).toEqual(
+    {
+      isAuthenticated: true,
+      error: '',
+      email: 'j@b.com',
+      token: '123',
+      user: {},
+    },
+  ));
+  it('handles LOGOUT', () => expect(
+    reducer(undefined, { type: 'LOGOUT' }),
+  ).toEqual(
+    {
+      isAuthenticated: false,
+      error: '',
+      email: '',
+      token: '',
+      user: {},
+    },
+  ));
+  it('handles AUTH_ERROR', () => expect(
+    reducer(undefined, {
+      type: 'AUTH_ERROR',
+      error: { message: 'bad' },
+    }),
+  ).toEqual(
+    {
+      isAuthenticated: false,
+      error: 'bad',
+      email: '',
+      token: '',
+      user: {},
+    },
+  ));
   it('handles SET_USER', () => {
     expect(
       reducer(undefined, {
