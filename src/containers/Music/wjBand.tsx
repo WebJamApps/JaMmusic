1017ec4010a56312e36abfdec360f6fe96931eda
Rhayven <codingforllamas@gmail.com>
2020-08-16 15:00:10 -0400

    Update to fix console warning...

--- src/containers/Music/wjBand.tsx
+++ src/containers/Music/wjBand.tsx
@@ -11,25 +11,27 @@ const WjBand = (): JSX.Element => (
       Recording sessions were completed at Flat Five Studios. The project ended after Brian moved to Austin, Tx.
     </p>
     <table>
-      <tr>
-        <td>
-          <div style={{ margin: 'auto', textAlign: 'center' }}>
-            <img src="/static/imgs/brianThumb.png" alt="Brian Lilienthal" height="100" />
-            <p>
-              Brian Lilienthal –
-              <strong><i>main</i></strong>
-              {' '}
-              drummer and remixer
-            </p>
-          </div>
-        </td>
-        <td>
-          <div style={{ margin: 'auto', textAlign: 'center' }}>
-            <img src="../static/imgs/emerson.jpg" alt="Emerson Harvey" height="100" />
-            <p>Emerson Harvey – lead guitarist and world traveler</p>
-          </div>
-        </td>
-      </tr>
+      <tbody>
+        <tr style={{ backgroundColor: '#fff' }}>
+          <td>
+            <div style={{ margin: 'auto', textAlign: 'center' }}>
+              <img src="/static/imgs/brianThumb.png" alt="Brian Lilienthal" height="100" />
+              <p>
+                Brian Lilienthal –
+                <strong><i>main</i></strong>
+                {' '}
+                drummer and remixer
+              </p>
+            </div>
+          </td>
+          <td>
+            <div style={{ margin: 'auto', textAlign: 'center' }}>
+              <img src="../static/imgs/emerson.jpg" alt="Emerson Harvey" height="100" />
+              <p>Emerson Harvey – lead guitarist and world traveler</p>
+            </div>
+          </td>
+        </tr>
+      </tbody>
     </table>
     <p style={{ height: '10px' }}>{' '}</p>
   </div>
