e1c253ab5ebf4650f535fb870fd420c251207cbf
Rhayven <codingforllamas@gmail.com>
2020-09-15 01:33:50 -0400

    CircleCI Fixes

--- test/redux/jsonTransform.spec.ts
+++ test/redux/jsonTransform.spec.ts
@@ -4,9 +4,8 @@ describe('jsonTransform', () => {
   it('transforms cirular json dependencies', () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const createTransform:any = (cb1: () => void, cb2: () => void) => { cb1(); cb2(); return true; };
-    const JSOG = { encode: jest.fn(), decode: jest.fn() };
     expect(typeof jsonTransform.makeTransform).toBe('function');
-    const ct = jsonTransform.makeTransform(createTransform, JSOG);
+    const ct = jsonTransform.makeTransform(createTransform);
     expect(ct).toBe(true);
   });
 });
