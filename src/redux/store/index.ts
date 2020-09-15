db3f07cd4000fada831d7a301b216c546c3964b8
Rhayven <codingforllamas@gmail.com>
2020-09-14 21:29:13 -0400

    Installed flatted and changed over from JSOG

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -2,12 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer, createTransform } from 'redux-persist';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
-import JSOG from 'jsog';
 import storageSession from 'redux-persist/lib/storage/session';
 import allReducers from '../allReducers';
 import jsonTransform from './jsonTransform';
 
-const JSOGTransform = jsonTransform.makeTransform(createTransform, JSOG);
+const JSOGTransform = jsonTransform.makeTransform(createTransform);
 const persistConfig = {
   key: 'root',
   storage: storageSession,
--- src/redux/store/jsonTransform.ts
+++ src/redux/store/jsonTransform.ts
@@ -1,11 +1,10 @@
 import type { createTransform, Transform } from 'redux-persist';
-import type jsog from 'jsog';
+import Flatted from 'flatted';
 
-const makeTransform = (ctf: typeof createTransform,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  JSOG: typeof jsog): Transform<unknown, unknown, any, unknown> => ctf(
-  (inboundState) => JSOG.encode(inboundState),
-  (outboundState) => JSOG.decode(outboundState),
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const makeTransform = (ctf: typeof createTransform): Transform<unknown, string, any, unknown> => ctf(
+  (inboundState) => Flatted.stringify(inboundState),
+  (outboundState) => Flatted.stringify(outboundState),
 );
 
 export default { makeTransform };
