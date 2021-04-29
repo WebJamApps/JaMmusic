9b810bb9e61d9d51d8eef7edcce4ed83fa73afb4
Logan Richards <logrdog15@gmail.com>
2021-04-29 04:18:33 -0400

    fix linter again

--- src/components/TourTable/DataTable.tsx
+++ src/components/TourTable/DataTable.tsx
@@ -9,12 +9,11 @@ type Props = {
 };
 export const DataTable = ({ columns, data }: Props):JSX.Element => {
   const { test } = useContext(TourTableContext);
-  // eslint-disable-next-line no-console
   const datearr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  const curdate: string = datearr[new Date().getMonth()] + ' ' + new Date().getDay() + ', ' + new Date().getFullYear();
-  let sorttours: Tour[] = data.filter(tour => new Date(tour.date) > new Date(curdate)).reverse();
-  sorttours[sorttours.length] = sorttours[sorttours.length+1];
-  sorttours = sorttours.concat(data.filter(tour => new Date(tour.date) < new Date(curdate)));
+  const curdate = datearr[new Date().getMonth()] + ' ' + new Date().getDay() + ', ' + new Date().getFullYear();
+  const sorttours = data.filter((tour) => new Date(tour.date) > new Date(curdate)).reverse();
+  sorttours[sorttours.length] = sorttours[sorttours.length + 1];
+  const sortedtours = [...sorttours,...data.filter((tour) => new Date(tour.date) < new Date(curdate))];
   return (
     <MUIDataTable
       options={{
@@ -30,7 +29,7 @@ export const DataTable = ({ columns, data }: Props):JSX.Element => {
         fixedHeader: false,
       }}
       columns={columns}
-      data={sorttours}
+      data={sortedtours}
       title=""
     />
   );
