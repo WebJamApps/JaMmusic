43f7dbdb7b5bfc59d9786b35ba9df11936adc388
Logan <logrdog15@gmail.com>
2021-08-09 01:06:40 -0400

    fix

--- src/components/PicSlider/index.tsx
+++ src/components/PicSlider/index.tsx
@@ -47,7 +47,7 @@ class PicSlider extends React.Component<PicSliderProps> {
             Array.isArray(data) ? data.map((d) => (
               <div key={d._id}>
                 {' '}
-                <img className="slide-images" src={d.url} alt={d.title}/>
+                <img className="slide-images" src={d.url} alt={d.title} />
                 {' '}
                 {d.comments === 'showCaption' ? <Caption caption={d.title} /> : null }
               </div>
