cabcbcaa3bdac618cf66222d1cdd34e2aad5623b
Joshua Sherman <joshua.v.sherman@gmail.com>
2021-10-29 13:56:40 -0400

    finishes unit tests

--- src/components/PhotoTable/index.tsx
+++ src/components/PhotoTable/index.tsx
@@ -1,37 +1,9 @@
 import React, { Dispatch } from 'react';
-import MUIDataTable, { MUIDataTableColumnDef } from 'mui-datatables';
-import HtmlReactParser from 'html-react-parser';
+import MUIDataTable from 'mui-datatables';
 import type { AnyAction } from 'redux';
 import type { Iimage } from '../../redux/mapStoreToProps';
 import type { MusicDashboardController } from '../../containers/MusicDashboard/MusicDashboardController';
-
-const setColumns = (): MUIDataTableColumnDef[] => {
-  const columns: MUIDataTableColumnDef[] = [];
-  const titles = ['Thumbnail', 'Title', 'Caption', 'Link', 'Type', 'Modify'];
-  for (let i = 0; i < titles.length; i += 1) {
-    const label = titles[i];// eslint-disable-line security/detect-object-injection
-    columns.push({
-      name: label.toLowerCase(),
-      label,
-      options: {
-        filter: false,
-        sort: true,
-        customBodyRender: (value:any) => {
-          if (typeof value !== 'string' && label !== 'Modify') value = '';
-          return (
-          <div style={{
-            margin: 0, fontSize: '12pt', maxWidth: '200px',
-          }}
-          >
-            {label !== 'Modify' ? HtmlReactParser(value) : value}
-          </div>
-          );
-        },
-      },
-    });
-  }
-  return columns;
-};
+import utils from './photoTable.utils';
 
 type PTprops = {
   dispatch: Dispatch<AnyAction>,
@@ -42,45 +14,8 @@ type PTprops = {
 
 export const PhotoTable = (props: PTprops):JSX.Element => {
 
-  const editPic = (data: Iimage): boolean => {
-    const { dispatch } = props;
-    delete data.modify;
-    dispatch({ type: 'EDIT_PIC', data });
-    return true;
-  };
-
-  const addThumbs = (arr: Iimage[]): Iimage[] => {
-    const { controller } = props;
-    const newArr = arr;/* eslint-disable security/detect-object-injection */
-    for (let i = 0; i < arr.length; i += 1) { // eslint-disable-next-line security/detect-object-injection
-      newArr[i].thumbnail = `<img src=${arr[i].url} width="200px"/>`;
-      const deletePicId = `deletePic${newArr[i]._id}`;// eslint-disable-line security/detect-object-injection
-      const editPicId = `editPic${newArr[i]._id}`;// eslint-disable-line security/detect-object-injection
-      newArr[i].link = `<a href=${arr[i].url} target="_blank">click to view</a>`;// eslint-disable-line security/detect-object-injection
-      newArr[i].caption = newArr[i].comments === 'showCaption' ? 'display' : 'hide';
-      newArr[i].modify = (// eslint-disable-line security/detect-object-injection
-        <div>
-          <button
-            type="button"
-            id={deletePicId}
-            onClick={() => {
-              controller.deleteData('deleteImage', newArr[i]._id);
-            }}
-          >
-            Delete Pic
-          </button>
-          <p>{' '}</p>
-          <button type="button" id={editPicId} onClick={() => { editPic(newArr[i]); }}>
-            Edit Pic
-          </button>
-        </div>
-      );
-    }
-    return newArr;
-  };
-
-  let arr: Iimage[] = props.images;
-  arr = addThumbs(arr);
+  let dataArr: Iimage[] = props.images;
+  dataArr = utils.addThumbs(dataArr, props.controller, props.dispatch);
   return (
       <div className="photoTable">
         <div style={{ maxWidth: '9in', margin: 'auto' }}>
@@ -97,8 +32,8 @@ export const PhotoTable = (props: PTprops):JSX.Element => {
               selectableRows: 'none',
               fixedHeader: false,
             }}
-            columns={setColumns()}
-            data={arr}
+            columns={utils.setColumns()}
+            data={dataArr}
             title=""
           />
         </div>
---
+++ test/components/PhotoTable/photoTable.utils.spec.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import utils from '../../../src/components/PhotoTable/photoTable.utils';
+// import React from 'react';
+import renderer from 'react-test-renderer';
+
+describe('photoTable.utils', ()=>{
+  it('setCustomBody when null value', ()=>{
+    const result = renderer.create(utils.setCustomBody(null, 'test')).root;
+    expect(result.findByType('div')).toBeDefined();
+  });
+  it('addThumbs when caption is hide and handles click on buttons', ()=>{
+    const imgArr:any[] = [{ comments:'', modify:null }];
+    const deleteData = jest.fn();
+    const arr:any[] = utils.addThumbs(imgArr, { deleteData }, jest.fn());
+    const buttons = renderer.create(arr[0].modify).root;
+    const allButtons = buttons.findAllByType('button');
+    allButtons[0].props.onClick();
+    expect(deleteData).toHaveBeenCalled();
+    expect(allButtons[1].props.onClick()).toBe(true);
+  });
+});
