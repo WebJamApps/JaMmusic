f9abf9c553339d5cba4ec08b08111751e86e279e
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-02-21 07:58:25 -0500

    still improving tooling

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-    presets: ["@babel/preset-react", "@babel/preset-env"]
+    presets: ["@babel/preset-react", "@babel/preset-env", "react-hot-loader/babel"]
   }
--- webpack.config.js
+++ webpack.config.js
@@ -32,9 +32,7 @@ if (process.env.BackendUrl === 'http://localhost:7000') {
 }
 module.exports = (env) => ({
   resolve: {
-    alias: {
-      src: srcDir
-    },
+    alias: { src: srcDir, 'react-dom': '@hot-loader/react-dom' },
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     fallback: { // needed for jsonwebtoken
       crypto: require.resolve('crypto-browserify'),
@@ -65,22 +63,22 @@ module.exports = (env) => ({
   },
 
   devServer: {
-    devMiddleware: {writeToDisk : true},
+    devMiddleware: { writeToDisk: true },
     static: outDir,
     hot: true,
     historyApiFallback: { // serve index.html for all 404 (required for push-state)
       rewrites: [{
-          from: /^\/$/,
-          to: '/'
-        },
-        {
-          from: /^\//,
-          to: '/'
-        },
-        {
-          from: /./,
-          to: '/'
-        },
+        from: /^\/$/,
+        to: '/'
+      },
+      {
+        from: /^\//,
+        to: '/'
+      },
+      {
+        from: /./,
+        to: '/'
+      },
       ],
     },
     port: parseInt(process.env.PORT, 10),
@@ -103,70 +101,70 @@ module.exports = (env) => ({
 
   module: {
     rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(ts|tsx)?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-      },
-      // SCSS required in JS/TS files should use the style-loader that auto-injects it into the website
-      // only when the issuer is a .js/.ts file, so the loaders are not applied inside html templates
-      {
-        test: /\.scss$/,
-        use: [
-          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader', // translates CSS into CommonJS
-          'sass-loader', // compiles Sass to CSS, using dart sass
-        ],
-      },
-      // Still needed for some node modules that use CSS
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader'
-      }, // eslint-disable-next-line no-useless-escape
-      // embed small images and fonts as Data Urls and larger ones as files:
-      {
-        test: /\.(png|gif|jpg|cur)$/i,
-        loader: 'url-loader',
-        options: {
-          limit: 8192
-        }
-      },
-      {
-        test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          mimetype: 'application/font-woff2'
-        }
-      },
-      {
-        test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          mimetype: 'application/font-woff'
-        }
-      },
-      // load these fonts normally, as files:
-      {
-        test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-        loader: 'file-loader'
-      },
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    },
+    {
+      test: /\.(ts|tsx)?$/,
+      loader: "ts-loader",
+      exclude: /node_modules/
+    },
+    {
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'source-map-loader',
+    },
+    // SCSS required in JS/TS files should use the style-loader that auto-injects it into the website
+    // only when the issuer is a .js/.ts file, so the loaders are not applied inside html templates
+    {
+      test: /\.scss$/,
+      use: [
+        process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader', // translates CSS into CommonJS
+        'sass-loader', // compiles Sass to CSS, using dart sass
+      ],
+    },
+    // Still needed for some node modules that use CSS
+    {
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader'],
+    },
+    {
+      test: /\.html$/i,
+      loader: 'html-loader'
+    }, // eslint-disable-next-line no-useless-escape
+    // embed small images and fonts as Data Urls and larger ones as files:
+    {
+      test: /\.(png|gif|jpg|cur)$/i,
+      loader: 'url-loader',
+      options: {
+        limit: 8192
+      }
+    },
+    {
+      test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        mimetype: 'application/font-woff2'
+      }
+    },
+    {
+      test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        mimetype: 'application/font-woff'
+      }
+    },
+    // load these fonts normally, as files:
+    {
+      test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+      loader: 'file-loader'
+    },
     ],
   },
 
@@ -195,13 +193,13 @@ module.exports = (env) => ({
     }),
     new CopyPlugin({
       patterns: [{
-          from: 'static/favicon.ico',
-          to: 'favicon.ico'
-        },
-        {
-          from: 'static/imgs',
-          to: 'static/imgs'
-        },
+        from: 'static/favicon.ico',
+        to: 'favicon.ico'
+      },
+      {
+        from: 'static/imgs',
+        to: 'static/imgs'
+      },
       ],
     }),
     new webpack.EnvironmentPlugin(envVars),
