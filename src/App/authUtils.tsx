d8116ada274ad158b144d5d9ef4bc5527a8a5e18
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-03-08 08:50:37 -0500

    fix reload

--- src/App/authUtils.tsx
+++ src/App/authUtils.tsx
@@ -6,8 +6,10 @@ import type { AppTemplate } from './AppTemplate';
 import type { Auth } from 'src/redux/mapStoreToProps';
 
 const setUserRedux = async (dispatch: (...args: any) => void, decoded: string | jwt.JwtPayload, auth: Auth) => {
-  if (typeof decoded !== 'string' && decoded.user) dispatch({ type: 'SET_USER', data: decoded.user });
-  else {
+  if (typeof decoded !== 'string' && decoded.user) {
+    dispatch({ type: 'SET_USER', data: decoded.user });
+    window.location.reload();
+  } else {
     try {
       const user = await superagent.get(`${process.env.BackendUrl}/user/${decoded.sub}`)
         .set('Accept', 'application/json').set('Authorization', `Bearer ${auth.token}`);
@@ -16,6 +18,7 @@ const setUserRedux = async (dispatch: (...args: any) => void, decoded: string | 
     } catch (e) { console.log(e); }
   }
 };
+
 export interface IauthUtils {
   setUser: (view: AppTemplate) => Promise<string>;
   responseGoogleLogin: (response: GoogleLoginResponseOffline | GoogleLoginResponse,
@@ -24,13 +27,13 @@ export interface IauthUtils {
   responseGoogleLogout: (dispatch: Dispatch<unknown>) => boolean;
 }
 const setUser = async (view: AppTemplate): Promise<void> => {
-  console.log('setUser');
   const { auth, dispatch } = view.props;
   try {
     const decoded = jwt.verify(auth.token, process.env.HashString || /* istanbul ignore next */'');
     await setUserRedux(dispatch, decoded, auth);
   } catch (e) { console.log(e); }
 };
+
 const responseGoogleLogin = async (response: GoogleLoginResponseOffline | GoogleLoginResponse,
   view: AppTemplate): Promise<void> => {
   const uri = window.location.href;
@@ -50,7 +53,9 @@ const responseGoogleLogin = async (response: GoogleLoginResponseOffline | Google
     await setUser(view);
   } catch (e) { console.log(e); }
 };
+
 const responseGoogleFailLogin = (response: unknown): string => `${response}`;
+
 const responseGoogleLogout = (dispatch: Dispatch<unknown>): boolean => {
   dispatch({ type: 'LOGOUT' });
   window.location.reload();
@@ -58,5 +63,5 @@ const responseGoogleLogout = (dispatch: Dispatch<unknown>): boolean => {
 };
 
 export default {
-  responseGoogleLogin, responseGoogleLogout, responseGoogleFailLogin, setUser,
+  responseGoogleLogin, responseGoogleLogout, responseGoogleFailLogin, setUser, setUserRedux,
 };
