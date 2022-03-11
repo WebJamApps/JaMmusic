20d6b1d8804d15d07ca218504bf46593bb3c73f6
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-03-11 08:51:03 -0500

    implements sorting for gigs

--- src/components/Gigs/index.tsx
+++ src/components/Gigs/index.tsx
@@ -2,21 +2,28 @@ import { useContext, useEffect, useState } from 'react';
 import { DataGrid, GridColumns, GridEnrichedColDef, GridRenderCellParams } from '@mui/x-data-grid';
 import { DataContext, IGig } from '../../providers/Data.provider';
 import HtmlReactParser from 'html-react-parser';
+import { defaultGig } from 'src/providers/fetchGigs';
 import './Gigs.scss';
 
-export const makeVenue = ():GridEnrichedColDef => {
+const makeVenueValue = (value:string) =>{
+  const parsed = HtmlReactParser(value);
+  if (value === 'Our Past Performances') return <span className="ourPastPerformances">{parsed}</span>;
+  return <span>{parsed}</span>;
+};
+
+export const makeVenue = (): GridEnrichedColDef => {
   return (
     {
       field: 'venue',
       headerName: 'Venue',
       width: 600,
       editable: false,
-      renderCell:(params:GridRenderCellParams) => <span>{HtmlReactParser(params.value)}</span>,
+      renderCell: (params: GridRenderCellParams) => makeVenueValue(params.value),
     }
   );
 };
 
-export const columns:GridColumns = [
+export const columns: GridColumns = [
   {
     field: 'date',
     headerName: 'Date',
@@ -41,14 +48,23 @@ export const columns:GridColumns = [
     headerName: 'Tickets',
     width: 140,
     editable: false,
-    renderCell:(params:GridRenderCellParams) => <span>{HtmlReactParser(params.value || 'Free')}</span>,
+    renderCell: (params: GridRenderCellParams) => <span>{HtmlReactParser(params.value || 'Free')}</span>,
   },
 ];
 
-const orderGigs = (gigs: IGig[], setGigsInOrder: { (arg0: IGig[]): void; }) => {
-  const futureGigs = gigs.filter((g)=> g.datetime && g.datetime >= new Date().toISOString());
+export const orderGigs = (gigs: IGig[], setGigsInOrder: { (arg0: IGig[]): void; }) => {
+  const now = new Date().toISOString();
+  const futureGigs = gigs.filter((g) => g.datetime && g.datetime >= now);
+  const pastGigs = gigs.filter((g) => g.datetime && g.datetime < now);
+  const sortedFuture = futureGigs.sort((a, b) => {
+    if (!a.datetime || !b.datetime) return 0;
+    if (a.datetime > b.datetime) return 1;
+    if (a.datetime < b.datetime) return -1;
+    return 0;
+  });
+  sortedFuture.push({ ...defaultGig, venue: 'Our Past Performances', id:999, tickets:' ' });
   console.log(futureGigs);
-  setGigsInOrder(gigs);
+  setGigsInOrder(sortedFuture.concat(pastGigs));
 };
 
 export const Gigs = (): JSX.Element => {
@@ -56,16 +72,16 @@ export const Gigs = (): JSX.Element => {
   const [gigsInOrder, setGigsInOrder] = useState(gigs);
   useEffect(() => orderGigs(gigs, setGigsInOrder), [gigs]);
   return (
-    <div style={{ margin: 'auto', padding:'10px', width:'100%' }}>
-      <h4 style={{ textAlign:'center' }}>Gigs</h4>
+    <div style={{ margin: 'auto', padding: '10px', width: '100%' }}>
+      <h4 style={{ textAlign: 'center' }}>Gigs</h4>
       <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={gigsInOrder}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        disableSelectionOnClick
-      />
+        <DataGrid
+          rows={gigsInOrder}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          disableSelectionOnClick
+        />
       </div>
     </div>
   );
