4f344417071f257d25409fdbf7fc0f8335007137
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-08-22 09:27:21 -0400

    finishes refactoring

--- src/App/AppTemplate/PageHost.tsx
+++ src/App/AppTemplate/PageHost.tsx
@@ -2,16 +2,16 @@ import { useState } from 'react';
 import { DrawerContainer } from './DrawerContainer';
 import { MainPanel } from './MainPanel';
 
-function toggleMobileMenu(menuOpen: boolean, setMenuOpen:(arg0:boolean)=>void): void {
+export function toggleMobileMenu(menuOpen: boolean, setMenuOpen:(arg0:boolean)=>void): void {
   const mO = !menuOpen;
   setMenuOpen(mO);
 }
 
-function handleKeyMenu(e: { key: string; }, menuOpen: boolean, setMenuOpen:(arg0:boolean)=>void): void {
+export function handleKeyMenu(e: { key: string; }, menuOpen: boolean, setMenuOpen:(arg0:boolean)=>void): void {
   if (e.key === 'Enter') toggleMobileMenu(menuOpen, setMenuOpen);
 }
 
-function handleKeyPress(e: { key: string; }, setMenuOpen:(arg0:boolean)=>void): (void) {
+export function handleEscapePress(e: { key: string; }, setMenuOpen:(arg0:boolean)=>void): (void) {
   if (e.key === 'Escape') setMenuOpen(false);
 }
 
@@ -20,14 +20,23 @@ export const makeDrawerClass = (menuOpen:boolean) => {
   return className;
 };
 
+export const makeHandleClose = (setMenuOpen:any) => () => setMenuOpen(false);
+
+export const makeOnClick = (menuOpen:boolean, setMenuOpen:any) => () => toggleMobileMenu(menuOpen, setMenuOpen);
+
+export const makeOnKeyPress = (menuOpen:boolean, setMenuOpen:any) => (evt:any) => handleKeyMenu(evt, menuOpen, setMenuOpen);
+
+export const makeHandleKeyPress = (setMenuOpen:any) => (evt:any) => handleEscapePress(evt, setMenuOpen);
+
 export function PageHost({ userCount, heartBeat, auth, location, dispatch, children }: any) {
   const [menuOpen, setMenuOpen ] = useState(false);
-  const handleClose  = () => setMenuOpen(false);
-  const onClick = () => toggleMobileMenu(menuOpen, setMenuOpen);
-  const onKeyPress = (evt:any) => handleKeyMenu(evt, menuOpen, setMenuOpen);
+  const handleClose  = makeHandleClose(setMenuOpen);
+  const onClick = makeOnClick(menuOpen, setMenuOpen);
+  const onKeyPress = makeOnKeyPress(menuOpen, setMenuOpen);
+  const handleKeyPress = makeHandleKeyPress(setMenuOpen);
   return (
         <div className="page-host">
-        <DrawerContainer handleKeyPress={(evt) => handleKeyPress(evt, setMenuOpen)} className={makeDrawerClass(menuOpen)}
+        <DrawerContainer handleKeyPress={handleKeyPress} className={makeDrawerClass(menuOpen)}
          userCount={userCount} heartBeat={heartBeat} auth={auth} location={location} 
          dispatch={dispatch} handleClose={handleClose}
          />
