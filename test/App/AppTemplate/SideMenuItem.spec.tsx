1d947370420c0474f4bd72443e50e3571350c075
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-08-23 08:36:32 -0400

    more unit tests

--- test/App/AppTemplate/SideMenuItem.spec.tsx
+++ test/App/AppTemplate/SideMenuItem.spec.tsx
@@ -1,84 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { continueMenuItem, SideMenuItem } from 'src/App/AppTemplate/SideMenuItem';
+import { BrowserRouter, RouteComponentProps } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import commonUtils from 'src/lib/commonUtils';
 import type { ImenuItem } from 'src/App/AppTemplate/menuConfig';
-import { BrowserRouter } from 'react-router-dom';
+import { continueMenuItem, SideMenuItem } from 'src/App/AppTemplate/SideMenuItem';
+import commonUtils from 'src/lib/commonUtils';
 import type { Auth } from 'src/redux/mapStoreToProps';
-import type { AppTemplateProps } from 'src/App/AppTemplate';
 
 describe('SideMenuItem', () => {
-  it('is defined', ()=>{
+  it('is defined', () => {
     expect(SideMenuItem).toBeDefined();
   });
-  // const vStub: any = {
-  //   googleButtons: () => true,
-  //   makeMenuLink: () => true,
-  //   props: {
-  //     location: { pathname: '/music' },
-  //     auth: { token: 'token', isAuthenticated: true, user: { userType: '' } },
-  //     dispatch: () => jest.fn(),
-  //   },
-  // };
-  // it('renders MenuItem when location.pathname is music', () => {
-  //   commonUtils.getUserRoles = jest.fn(()=>['']);
-  //   const props = {
-  //     menu: { auth: true, link: '/music' } as ImenuItem, index: 1, view: vStub,
-  //   };
-  //   const menuItem:any = renderer.create(
-  //   <GoogleOAuthProvider clientId=""><BrowserRouter>
-  //   <SideMenuItem {...props} /></BrowserRouter></GoogleOAuthProvider>).toJSON();
-  //   expect(menuItem.type).toBe('div');
-  // });
-  // it('renders MenuItem when location.pathname is music for Web Jam LLC', () => {
-  //   commonUtils.getUserRoles = jest.fn(()=>['']);
-  //   const props = {
-  //     menu: { auth: true, link: '/test', name:'Web Jam LLC' } as ImenuItem, index: 1, view: vStub,
-  //   };
-  //   const menuItem:any = renderer.create(<BrowserRouter><SideMenuItem {...props} /></BrowserRouter>).toJSON();
-  //   expect(menuItem.type).toBe('div');
-  // });
-  // it('renders MenuItem when location.pathname is not music', () => {
-  //   commonUtils.getUserRoles = jest.fn(()=>['']);
-  //   vStub.props.location.pathname = 'test';
-  //   const props = {
-  //     menu: { auth: true, link: '/test', type:'link' } as ImenuItem, index: 1, view: vStub,
-  //   };
-  //   const menuItem:any = renderer.create(<BrowserRouter><SideMenuItem {...props} /></BrowserRouter>).toJSON();
-  //   expect(menuItem.type).toBe('div');
-  // });
-  // it('renders MenuItem when googleLogout', () => {
-  //   commonUtils.getUserRoles = jest.fn(()=>['']);
-  //   vStub.props.location.pathname = 'test';
-  //   const props = {
-  //     menu: { auth: true, link: '/test', type:'googleLogout' } as ImenuItem, index: 1, view: vStub,
-  //   };
-  //   const menuItem:any = renderer.create(
-  //     <GoogleOAuthProvider clientId=""><BrowserRouter>
-  //     <SideMenuItem {...props} /></BrowserRouter></GoogleOAuthProvider>).toJSON();
-  //   expect(menuItem.type).toBe('div');
-  // });
-  // it('returns null', ()=>{
-  //   commonUtils.getUserRoles = jest.fn(()=>['test']);
-  //   vStub.props.location.pathname = 'test';
-  //   const props = {
-  //     menu: { auth: true, link: '/test', type:'googleLogout' } as ImenuItem, index: 1, view: vStub,
-  //   };
-  //   const menuItem:any = renderer.create(<BrowserRouter><SideMenuItem {...props} /></BrowserRouter>).toJSON();
-  //   expect(menuItem).toBeNull();
-  // });
-  // it('continueMenuItem returns googleLogin', ()=>{
-  //   const cMenuItem:any = continueMenuItem(
-  //     { type:'googleLogin' } as ImenuItem, 1, { isAuthenticated:false } as Auth, {} as AppTemplateProps);
-  //   const result:any = renderer.create(
-  //   <GoogleOAuthProvider clientId="">cMenuItem</GoogleOAuthProvider>).toJSON();
-  //   console.log(result);
-  // });
-  // it('continueMenuItem returns null', ()=>{
-  //   const cMenuItem:any = continueMenuItem(
-  //     { type:'' } as ImenuItem, 1, { isAuthenticated:false } as Auth, {} as AppTemplateProps);
-  //   const result:any = renderer.create(cMenuItem).toJSON();
-  //   expect(result).toBeNull();
-  // });
+  it('returns null for auth item when user is incorrect type', () => {
+    commonUtils.getUserRoles = jest.fn(() => ['tester']);
+    const props = {
+      index: 1, auth: { isAuthenticated: true, user: { userType: 'joker' } } as Auth,
+      location: { pathname: '/music' } as RouteComponentProps['location'],
+      menu: { auth: true, link: '/dashboard' } as ImenuItem, dispatch: jest.fn(), handleClose: jest.fn(),
+    };
+    const smi = renderer.create(<SideMenuItem {...props} />).toJSON();
+    expect(smi).toBeNull();
+  });
+  it('returns MakeLink for /music', () => {
+    commonUtils.getUserRoles = jest.fn(() => ['tester']);
+    const props = {
+      index: 1, auth: { isAuthenticated: false, user: { userType: 'joker' } } as Auth,
+      location: { pathname: '/music' } as RouteComponentProps['location'],
+      menu: { auth: false, link: '/music' } as ImenuItem, dispatch: jest.fn(), handleClose: jest.fn(),
+    };
+    const smi: any = renderer.create(<BrowserRouter><SideMenuItem {...props} /></BrowserRouter>).toJSON();
+    expect(smi.props.className).toBe('menu-item');
+  });
+  it('returns MakeLink for /music when Web Jam LLC', () => {
+    commonUtils.getUserRoles = jest.fn(() => ['tester']);
+    const props = {
+      index: 1, auth: { isAuthenticated: false, user: { userType: 'joker' } } as Auth,
+      location: { pathname: '/music' } as RouteComponentProps['location'],
+      menu: { auth: false, link: '/', name:'Web Jam LLC' } as ImenuItem, dispatch: jest.fn(), handleClose: jest.fn(),
+    };
+    const smi: any = renderer.create(<BrowserRouter><SideMenuItem {...props} /></BrowserRouter>).toJSON();
+    expect(smi.props.className).toBe('menu-item');
+  });
+  it('continueMenuItem returns googleLogout', () => {
+    const result:any = continueMenuItem({ type: 'googleLogout' } as ImenuItem, 1, { isAuthenticated: true } as Auth, '', jest.fn());
+    expect(result.key).toBe('googleLogout');
+  });
 });
