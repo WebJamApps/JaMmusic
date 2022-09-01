c0084b90d2ea84e2233564d0367275aa4c1942d2
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-09-01 09:51:45 -0400

    rev version

--- src/redux/mapStoreToAppTemplateProps.ts
+++ src/redux/mapStoreToAppTemplateProps.ts
@@ -4,7 +4,7 @@ import type { Store } from './mapStoreToProps';
 interface TStore extends Store {
   sc: { scc: AGClientSocket; userCount: number, heartBeat: string };
 }
-const mapStoreToProps = (store: any): any => ({
+const mapStoreToProps = (store: TStore): Record<string, unknown> => ({
   images: store.images.images,
   heartBeat: store.sc.heartBeat,
   userCount: store.sc.userCount,
