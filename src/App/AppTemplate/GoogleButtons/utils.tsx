b64b13c3b83b4ed8caa54430cd186ced9f15ec79
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-09-07 08:23:38 -0400

    fixes jest

--- src/App/AppTemplate/GoogleButtons/utils.tsx
+++ src/App/AppTemplate/GoogleButtons/utils.tsx
@@ -11,22 +11,20 @@ export interface GoogleBody {
   state(): string,
 }
 
-const setUserRedux = async (dispatch: (...args: any) => void, token: string, userId: string) => {
-  try {
-    const user = await superagent.get(`${process.env.BackendUrl}/user/${userId}`)
-      .set('Accept', 'application/json').set('Authorization', `Bearer ${token}`);
-    dispatch({ type: 'SET_USER', data: user.body });
-    window.location.reload();
-  } catch (e) { console.log(e); }
+const setUserRedux = async (
+  dispatch: (...args: any) => void,
+  token: string,
+  userId: string | undefined,
+) => {
+  const user = await superagent.get(`${process.env.BackendUrl}/user/${userId}`)
+    .set('Accept', 'application/json').set('Authorization', `Bearer ${token}`);
+  dispatch({ type: 'SET_USER', data: user.body });
+  window.location.reload();
 };
 
 const setUser = async (dispatch: Dispatch<unknown>, token:string): Promise<void> => {
-  let userId;
-  try {
-    const { sub } = jwt.verify(token, process.env.HashString /* istanbul ignore next */|| '') as jwt.JwtPayload;
-    userId = sub;
-  } catch (e) { console.log(e); }
-  await setUserRedux(dispatch, token, userId || '');
+  const { sub } = jwt.verify(token, process.env.HashString as string) as jwt.JwtPayload;
+  await setUserRedux(dispatch, token, sub);
 };
 
 const gotToken = (doc: unknown): { type: string; data: unknown } => ({
@@ -34,7 +32,10 @@ const gotToken = (doc: unknown): { type: string; data: unknown } => ({
   data: doc,
 });
 
-const authenticate = async (googleBody: GoogleBody, dispatch:Dispatch<unknown>): Promise<{ token:string, email:string }> => {
+const authenticate = async (
+  googleBody: GoogleBody,
+  dispatch:Dispatch<unknown>,
+): Promise<{ token:string, email:string }> => {
   const { body } = await superagent.post(`${process.env.BackendUrl}/user/auth/google`)
     .set({ Accept: 'application/json' }).send(googleBody);
   dispatch(gotToken(body));
@@ -49,21 +50,22 @@ const makeState = () => () => {
 const responseGoogleLogin = async (
   response: Omit<CodeResponse, 'error' | 'error_description' | 'error_uri'>,
   dispatch: Dispatch<unknown>,
-): Promise<void> => {
-  const uri = window.location.href;
-  const baseUri = uri.split('/')[2];
-  const body = {
-    clientId: process.env.GoogleClientId,
-    redirectUri: !baseUri.includes('localhost')
-    && process.env.NODE_ENV === 'production' /* istanbul ignore next */? `https://${baseUri}`
-      : `http://${baseUri}`,
-    code: `${response.code}`,
-    state: makeState(),
-  };
+): Promise<boolean> => {
   try {
+    const uri = window.location.href;
+    const baseUri = uri.split('/')[2];
+    const body = {
+      clientId: process.env.GoogleClientId,
+      redirectUri: !baseUri.includes('localhost')
+    && process.env.NODE_ENV === 'production' ? `https://${baseUri}`
+        : `http://${baseUri}`,
+      code: `${response.code}`,
+      state: makeState(),
+    };
     const { token } = await authenticate(body, dispatch);
     await setUser(dispatch, token);
-  } catch (e) { console.log(e); }
+    return true;
+  } catch (e) { console.log(e); return false; }// TODO add error notification here
 };
 
 const responseGoogleLogout = async (dispatch: Dispatch<unknown>): Promise<void> => {
