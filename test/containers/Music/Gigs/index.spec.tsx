ee1dce8d035c848e493ccda8d7c0a205e97736a1
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-10-17 07:50:57 -0400

    more unit tests

--- test/containers/Music/Gigs/index.spec.tsx
+++ test/containers/Music/Gigs/index.spec.tsx
@@ -2,7 +2,7 @@
 import renderer from 'react-test-renderer';
 import type { IGig } from 'src/providers/Data.provider';
 import {
-  Gigs, makeVenue, columns, makeVenueValue, orderGigs,
+  Gigs, makeVenue, columns, makeVenueValue, orderGigs, checkDisabled,
 } from 'src/containers/Music/Gigs';
 
 describe('Gigs', () => {
@@ -47,6 +47,18 @@ describe('Gigs', () => {
     const cell:any = tickets.renderCell(params);
     expect(cell.type).toBe('span');
   });
+  it('makeDateValue', () => {
+    const date:any = columns[0];
+    const params:any = { row: { datetime: new Date().toISOString() } };
+    const cell:any = date.renderCell(params);
+    expect(typeof cell).toBe('string');
+  });
+  it('makeTimeValue', () => {
+    const time:any = columns[0];
+    const params:any = { row: { datetime: new Date().toISOString() } };
+    const cell:any = time.renderCell(params);
+    expect(typeof cell).toBe('string');
+  });
   it('makeVenueValue when Our Past Performances', () => {
     const result = makeVenueValue('Our Past Performances');
     expect(result.type).toBe('span');
@@ -67,5 +79,31 @@ describe('Gigs', () => {
     const gigs = [{ datetime: tomorrow }, { datetime: yesterday }, { datetime: future }, { datetime: today }, { datetime: tomorrow }] as IGig[];
     orderGigs(gigs, setGigsInOrder, jest.fn());
     expect(setGigsInOrder).toHaveBeenCalled();
+  });
+  it('properly sets the order for gigs when we have many future gigs', () => {
+    const setGigsInOrder = jest.fn();
+    const setPageSize = jest.fn();
+    const today = new Date().toISOString();
+    let tomorrow:any, future:any, yesterday:any;
+    yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+    yesterday = new Date(yesterday).toISOString();
+    tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    tomorrow = new Date(tomorrow).toISOString();
+    future = new Date(tomorrow);
+    future.setDate(future.getDate() + 2);
+    future = new Date(future).toISOString();
+    const gigs = [
+      { datetime: future }, { datetime: future },
+      { datetime: future }, { datetime: future }, { datetime: future },
+      { datetime: future }, { datetime: future }, { datetime: tomorrow },
+      { datetime: yesterday }, { datetime: future }, { datetime: today }, { datetime: tomorrow },
+    ] as IGig[];
+    orderGigs(gigs, setGigsInOrder, setPageSize);
+    expect(setPageSize).toHaveBeenCalledWith(12);
+  });
+  it('checkDisabled when false', () => {
+    expect(checkDisabled('city', 'state', new Date(), 'venue')).toBe(false);
   });
 });
