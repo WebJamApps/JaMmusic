54787b284f9c40505ad7bcbc3d7d68d66bf1d4ef
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-11-29 08:26:34 -0500

    more unit testing

--- src/App/PrivateRoute.tsx
+++ src/App/PrivateRoute.tsx
@@ -1,38 +1,24 @@
 
-import { useContext, useState, useEffect } from 'react';
+import { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { AuthContext, Iauth } from 'src/providers/Auth.provider';
 import commonUtils from '../lib/commonUtils';
 
-interface IpRouteProps {
-  auth: Iauth, userRoles: string[], path: string, Container: React.ElementType,
-}
-const PRoute = (props: IpRouteProps) => {
-  const {
-    auth, userRoles, path, Container,
-  } = props;
+// eslint-disable-next-line react/display-name
+export const makeRender = (auth:Iauth, userRoles:string[], Container: React.ElementType) => () => {
   const isAllowed = !!(auth.isAuthenticated && auth.user.userType && userRoles.indexOf(auth.user.userType) !== -1);
-  return (
-    <Route
-      path={path}
-      render={() => (isAllowed ? (
-        <Container />
-      ) : (
-        <Redirect to="/" />
-      ))}
-    />
-  );
+  if (isAllowed) return <Container />;
+  return <Redirect to="/" />;
 };
+
 interface PProps {
   Container: React.ElementType,
   path: string
 }
 export function PrivateRoute({ Container, path }: PProps): JSX.Element {
   const { auth } = useContext(AuthContext);
-  const [userRoles, setUserRoles] = useState([] as string[]);
-  useEffect(() => {
-    setUserRoles(commonUtils.getUserRoles());
-  }, []);
-  return <PRoute auth={auth} userRoles={userRoles} path={path} Container={Container} />;
+  const userRoles = commonUtils.getUserRoles();
+  const render = makeRender(auth, userRoles, Container);
+  return <Route path={path} render={render} />;
 }
 
--- test/App/PrivateRoute.spec.tsx
+++ test/App/PrivateRoute.spec.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { BrowserRouter } from 'react-router-dom';
-import { PrivateRoute } from 'src/App/PrivateRoute';
+import { PrivateRoute, makeRender } from 'src/App/PrivateRoute';
 import GoogleMap from 'src/containers/GoogleMap';
-// import commonUtils from 'src/lib/commonUtils';
-// import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import ReactDom from 'react-dom';
 import { act } from 'react-dom/test-utils';
+import { Music } from 'src/containers/Music';
 
 describe('PrivateRoute', () => {
   it('renders and runs useEffect', () => {
@@ -21,5 +21,15 @@ describe('PrivateRoute', () => {
     const gMap:any = document.getElementById('googleMap');
     expect(gMap).toBeDefined();
     document.body.removeChild(container);
+  });
+  it('makeRender returns the Container', () => {
+    const Mr = makeRender({ isAuthenticated: true, user: { userType: 'test' } } as any, ['test'], Music);
+    const rendered:any = renderer.create(<Mr />).toJSON();
+    expect(rendered.props.className).toBe('page-content');
+  });
+  it('makeRender returns the Redirect', () => {
+    const Mr = makeRender({ isAuthenticated: true, user: { userType: 'test' } } as any, ['test2'], Music);
+    const rendered:any = renderer.create(<BrowserRouter><Mr /></BrowserRouter>).toJSON();
+    expect(rendered).toBeNull();
   });
 });
