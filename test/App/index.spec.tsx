f52448671451323fb12e263892733c74fca78378
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-11-30 07:42:14 -0500

    unit tests

--- test/App/index.spec.tsx
+++ test/App/index.spec.tsx
@@ -1,11 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import env from 'dotenv';
 import { App } from 'src/App';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from 'src/redux/store/index';
 
 env.config();
 describe('App component', () => {
   it('is defined', () => {
     expect(App).toBeDefined();
+  });
+  it('renders', () => {
+    const props = {
+      dispatch: jest.fn(), images: [], showMap: false,
+    };
+    const app:any = renderer.create(<Provider store={store.store}><App {...props}><div /></App></Provider>).toJSON();
+    expect(app.props.className).toBe('App');
   });
   it('renders Google Map API when localhost', () => {
     process.env.BackendUrl = 'http://localhost:7000';
--- test/components/MusicPlayer/index.spec.tsx
+++ test/components/MusicPlayer/index.spec.tsx
@@ -1,10 +1,15 @@
 
 import { MusicPlayer } from 'src/components/MusicPlayer';
+import renderer from 'react-test-renderer';
 import TSongs from '../../testSongs';
 
 describe('Music player component init', () => {
   it('is defined', () => {
     expect(MusicPlayer).toBeDefined();
     expect(TSongs).toBeDefined();
+  });
+  it('renders', () => {
+    const mp:any = renderer.create(<MusicPlayer songs={[]} filterBy="originals" />).toJSON();
+    expect(mp.props.className).toBe('container-fluid');
   });
 });
