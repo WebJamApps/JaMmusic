a7e953f58de818710c7d1ddbda19a9b8513bdeb4
Joshua Sherman <joshua.v.sherman@gmail.com>
2022-12-06 07:55:54 -0500

    unit test

--- test/App/index.spec.tsx
+++ test/App/index.spec.tsx
@@ -4,6 +4,7 @@ import { App, LoadMap, HomeOrMusic } from 'src/App';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import store from 'src/redux/store/index';
+import { BrowserRouter } from 'react-router-dom';
 
 env.config();
 describe('App component', () => {
@@ -17,10 +18,6 @@ describe('App component', () => {
     const app:any = renderer.create(<Provider store={store.store}><App {...props}><div /></App></Provider>).toJSON();
     expect(app.props.className).toBe('App');
   });
-  it('renders Google Map API when localhost', () => {
-    process.env.BackendUrl = 'http://localhost:7000';
-    expect('/map').toBeDefined();
-  });
   it('HomeOrMusic when music', () => {
     const hOm:any = renderer.create(<HomeOrMusic appName="JaM" images={[]} />).toJSON();
     expect(hOm.props.className).toBe('page-content music');
@@ -28,5 +25,9 @@ describe('App component', () => {
   it('LoadMap when null', () => {
     const lm:any = renderer.create(<LoadMap backendUrl="https://web-jam.com" />).toJSON();
     expect(lm).toBeNull();
+  });
+  it('LoadMap when GoogleMap', () => {
+    const lm:any = renderer.create(<BrowserRouter><LoadMap backendUrl="http://localhost:7000" /></BrowserRouter>).root;
+    expect(lm.findByProps({ className: 'page-content google' })).toBeDefined();
   });
 });
