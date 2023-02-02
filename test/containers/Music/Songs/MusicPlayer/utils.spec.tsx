d6a815a09edf603d0ed47803ba2af1f6cc7a8ee7
Joshua Sherman <joshua.v.sherman@gmail.com>
2023-02-02 08:58:59 -0500

    cleanup and increment version

--- test/containers/Music/Songs/MusicPlayer/utils.spec.tsx
+++ test/containers/Music/Songs/MusicPlayer/utils.spec.tsx
@@ -6,14 +6,4 @@ describe('MusicPlayer/utils', () => {
   it('is defined', () => {
     expect(utils).toBeDefined();
   });
-  // it('makes text under player when empty song object', () => {
-  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //   const song:any = {};
-  //   const result = musicUtils.textUnderPlayer(song);
-  //   expect(result).toBeDefined();
-  // });
-  // it('setIndex', () => {
-  //   const result = musicUtils.setIndex(TSongs, 'Original');
-  //   expect(result.length).toBeGreaterThan(0);
-  // });
 });
