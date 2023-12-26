5494c99fca4811766c7c0667e8dfcc30ed4b65df
JoshuaVSherman <joshua.v.sherman@gmail.com>
2023-12-26 15:31:26 -0500

    adds apollo client

--- src/containers/SortContainer/index.tsx
+++ src/containers/SortContainer/index.tsx
@@ -1,13 +1,34 @@
+import { gql, useQuery } from '@apollo/client';
 
 function makeRandomString(): string {
   const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   const withNoDigits = randomString.replace(/[0-9]/g, 'X');
   return withNoDigits;
 }
+
+export const TEST_GQL = gql`
+  query Gqldocs {
+    gqldocs {
+      title
+      author
+    }
+  }
+`;
 export function SortContainer(): JSX.Element {
+  const { loading, error, data } = useQuery(TEST_GQL);
   const dice = Array.from({ length: 6 }, () => Math.floor(Math.random() * 6));
   const sorted = [...dice].sort();
   const randomString = makeRandomString();
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    return (
+      <p>
+        Error :
+        {' '}
+        {error.message}
+      </p>
+    );
+  }
   return (
     <div id="sort-container" style={{ margin: 'auto', textAlign: 'center' }}>
       <h4>Sorting Example</h4>
@@ -31,6 +52,7 @@ export function SortContainer(): JSX.Element {
           {randomString.split('').sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' })).join('')}
         </strong>
       </p>
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 }
--- test/containers/SortContainer/index.spec.tsx
+++ test/containers/SortContainer/index.spec.tsx
@@ -1,9 +1,25 @@
-import { SortContainer } from 'src/containers/SortContainer';
+import { SortContainer, TEST_GQL } from 'src/containers/SortContainer';
 import renderer from 'react-test-renderer';
+import { MockedProvider } from '@apollo/client/testing';
 
 describe('SortContainer', () => {
-  it('renders correctly', () => {
-    const sc:any = renderer.create(<SortContainer />).toJSON();
-    expect(sc.props.id).toBe('sort-container');
+  it('renders loading...', () => {
+    const mocks = [
+      {
+        request: {
+          query: TEST_GQL,
+          variables: {
+            name: 'Buck',
+          },
+        },
+        result: {
+          data: {
+            dog: { id: '1', name: 'Buck', breed: 'bulldog' },
+          },
+        },
+      },
+    ];
+    const sc:any = renderer.create(<MockedProvider mocks={mocks} addTypename={false}><SortContainer /></MockedProvider>).toJSON();
+    expect(sc.children[0]).toBe('Loading...');
   });
 });
