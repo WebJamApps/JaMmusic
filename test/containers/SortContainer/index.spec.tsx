787376f84075993efbbe414a083e44c72874ab74
JoshuaVSherman <joshua.v.sherman@gmail.com>
2024-02-26 07:50:36 -0500

    adds unit tests

--- test/containers/SortContainer/index.spec.tsx
+++ test/containers/SortContainer/index.spec.tsx
@@ -1,6 +1,7 @@
-import { SortContainer, TEST_GQL } from 'src/containers/SortContainer';
-import renderer from 'react-test-renderer';
+import { SortContainer, SortSection, TEST_GQL } from 'src/containers/SortContainer';
+import renderer, { ReactTestRendererJSON } from 'react-test-renderer';
 import { MockedProvider } from '@apollo/client/testing';
+import { ApolloError } from '@apollo/client';
 
 describe('SortContainer', () => {
   it('renders loading...', () => {
@@ -21,5 +22,19 @@ describe('SortContainer', () => {
     ];
     const sc:any = renderer.create(<MockedProvider mocks={mocks} addTypename={false}><SortContainer /></MockedProvider>).toJSON();
     expect(sc.children[0]).toBe('Loading...');
+  });
+  it('renders SortSection when error', () => {
+    const props = {
+      error: new ApolloError({ graphQLErrors: ['failed' as any] }), data: {}, dice: [], sorted: [], randomString: '', loading: false,
+    };
+    const ss = renderer.create(<SortSection {...props} />).toJSON() as ReactTestRendererJSON;
+    expect(ss.type).toBe('p');
+  });
+  it('renders SortSection as div', () => {
+    const props = {
+      error: undefined, data: {}, dice: [], sorted: [], randomString: '', loading: false,
+    };
+    const ss = renderer.create(<SortSection {...props} />).toJSON() as ReactTestRendererJSON;
+    expect(ss.type).toBe('div');
   });
 });
