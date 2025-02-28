91a9c409c14396e8e575fcd330748add4f83ff11
Tim <timsherman75@gmail.com>
2025-02-28 15:01:10 -0500

    horizontal line

--- src/containers/Songs/MusicPlayer/index.tsx
+++ src/containers/Songs/MusicPlayer/index.tsx
@@ -54,51 +54,54 @@ export function MyButtons(props: ImyButtonsProps): JSX.Element {
     playing, setPlaying, index, songsState, setIndex, isSingle,
   } = props;
   return (
-    <div style={{ paddingTop: 0, margin: 'auto' }}>
-      <div id="play-buttons">
-        <Button
-          size="small"
-          variant="contained"
-          id="play-pause"
-          className={playing ? 'on' : 'off'}
-          onClick={() => utils.play(playing, setPlaying)}
-        >
-          Play/Pause
-        </Button>
-        {!isSingle ? null : (
+    <>
+      <div style={{ paddingTop: 0, margin: 'auto' }}>
+        <div id="play-buttons">
           <Button
             size="small"
-            variant="outlined"
-            id="home"
-            onClick={() => window.open('https://web-jam.com/music/songs', '_blank')}
+            variant="contained"
+            id="play-pause"
+            className={playing ? 'on' : 'off'}
+            onClick={() => utils.play(playing, setPlaying)}
           >
-            More Songs
+            Play/Pause
           </Button>
-        )}
-        {isSingle ? null
-          : (
-            <>
-              <Button
-                size="small"
-                variant="outlined"
-                id="prev"
-                onClick={() => utils.prev(index, songsState, setIndex)}
-              >
-                Prev
-              </Button>
-
-              <Button
-                size="small"
-                variant="outlined"
-                id="next"
-                onClick={() => utils.next(index, songsState, setIndex)}
-              >
-                Next
-              </Button>
-            </>
+          {!isSingle ? null : (
+            <Button
+              size="small"
+              variant="outlined"
+              id="home"
+              onClick={() => window.open('https://web-jam.com/music/songs', '_blank')}
+            >
+              More Songs
+            </Button>
           )}
+          {isSingle ? null
+            : (
+              <>
+                <Button
+                  size="small"
+                  variant="outlined"
+                  id="prev"
+                  onClick={() => utils.prev(index, songsState, setIndex)}
+                >
+                  Prev
+                </Button>
+
+                <Button
+                  size="small"
+                  variant="outlined"
+                  id="next"
+                  onClick={() => utils.next(index, songsState, setIndex)}
+                >
+                  Next
+                </Button>
+              </>
+            )}
+        </div>
       </div>
-    </div>
+      <hr className="horizontal-line" />
+    </>
   );
 }
 
